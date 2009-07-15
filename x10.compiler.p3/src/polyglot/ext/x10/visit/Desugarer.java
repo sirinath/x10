sirinath/x10

commit 2f66b7f27bed8da8b8ba87bb212b10354a4daf4c

Remove names from asyncs.  This naive implementation was serialising strings with every async, causing a 7x bloat in a simple example i just tried.
If we want to name asyncs, we should do so in a way which does not cause extra
communication.  Since names are static, they are available at every place from
the get-go.  The easiest way to do this is to define toString() on closures.
Closures in the c++ backend already have a toString() function which returns
file:line information.



git-svn-id: svn://svn.code.sf.net/p/x10/code/trunk@10695 a326200e-df2c-42e0-81fa-597e909af41d

x10.compiler.p3/src/polyglot/ext/x10/visit/Desugarer.java
@@ -234,16 +234,15 @@ public class Desugarer extends ContextVisitor {
         return closure;
     }
 
-    private Stmt async(Position pos, Stmt body, List clocks, Expr place, String prefix) throws SemanticException {
-        if (clocks.size() == 0) return async(pos, body, place, prefix);
+    private Stmt async(Position pos, Stmt body, List clocks, Expr place) throws SemanticException {
+        if (clocks.size() == 0) return async(pos, body, place);
         Type clockRailType = xts.ValRail(xts.Clock());
         Tuple clockRail = (Tuple) xnf.Tuple(pos, clocks).type(clockRailType);
         Block block = body instanceof Block ? (Block) body : xnf.Block(body.position(), body);
         Closure closure = closure(body.position(), xts.Void(), Collections.EMPTY_LIST, block);
-        StringLit pString = (StringLit) xnf.StringLit(pos, prefix + pos.nameAndLineString()).type(xts.String());
-        List<Expr> args = Arrays.asList(new Expr[] { place, clockRail, closure, pString });
+        List<Expr> args = Arrays.asList(new Expr[] { place, clockRail, closure });
         List<Type> mArgs = Arrays.asList(new Type[] {
-            xts.Place(), clockRailType, closure.closureDef().asType(), xts.String()
+            xts.Place(), clockRailType, closure.closureDef().asType()
         });
         // TODO: merge with the call() function
         MethodInstance implMI = xts.findMethod(xts.Runtime(),
@@ -253,13 +252,12 @@ public class Desugarer extends ContextVisitor {
                 args).methodInstance(implMI).type(xts.Void()));
     }
 
-    private Stmt async(Position pos, Stmt body, Expr place, String prefix) throws SemanticException {
+    private Stmt async(Position pos, Stmt body, Expr place) throws SemanticException {
         Block block = body instanceof Block ? (Block) body : xnf.Block(body.position(), body);
         Closure closure = closure(body.position(), xts.Void(), Collections.EMPTY_LIST, block);
-        StringLit pString = (StringLit) xnf.StringLit(pos, prefix + pos.nameAndLineString()).type(xts.String());
-        List<Expr> args = Arrays.asList(new Expr[] { place, closure, pString });
+        List<Expr> args = Arrays.asList(new Expr[] { place, closure });
         List<Type> mArgs = Arrays.asList(new Type[] {
-            xts.Place(), closure.closureDef().asType(), xts.String()
+            xts.Place(), closure.closureDef().asType()
         });
         // TODO: merge with the call() function
         MethodInstance implMI = xts.findMethod(xts.Runtime(),
@@ -269,16 +267,15 @@ public class Desugarer extends ContextVisitor {
                 args).methodInstance(implMI).type(xts.Void()));
     }
 
-    private Stmt async(Position pos, Stmt body, List clocks, String prefix) throws SemanticException {
-        if (clocks.size() == 0) return async(pos, body, prefix);
+    private Stmt async(Position pos, Stmt body, List clocks) throws SemanticException {
+        if (clocks.size() == 0) return async(pos, body);
         Type clockRailType = xts.ValRail(xts.Clock());
         Tuple clockRail = (Tuple) xnf.Tuple(pos, clocks).type(clockRailType);
         Block block = body instanceof Block ? (Block) body : xnf.Block(body.position(), body);
         Closure closure = closure(body.position(), xts.Void(), Collections.EMPTY_LIST, block);
-        StringLit pString = (StringLit) xnf.StringLit(pos, prefix + pos.nameAndLineString()).type(xts.String());
-        List<Expr> args = Arrays.asList(new Expr[] { clockRail, closure, pString });
+        List<Expr> args = Arrays.asList(new Expr[] { clockRail, closure });
         List<Type> mArgs = Arrays.asList(new Type[] {
-            clockRailType, closure.closureDef().asType(), xts.String()
+            clockRailType, closure.closureDef().asType()
         });
         // TODO: merge with the call() function
         MethodInstance implMI = xts.findMethod(xts.Runtime(),
@@ -288,13 +285,12 @@ public class Desugarer extends ContextVisitor {
                 args).methodInstance(implMI).type(xts.Void()));
     }
 
-    private Stmt async(Position pos, Stmt body, String prefix) throws SemanticException {
+    private Stmt async(Position pos, Stmt body) throws SemanticException {
         Block block = body instanceof Block ? (Block) body : xnf.Block(body.position(), body);
         Closure closure = closure(body.position(), xts.Void(), Collections.EMPTY_LIST, block);
-        StringLit pString = (StringLit) xnf.StringLit(pos, prefix + pos.nameAndLineString()).type(xts.String());
-        List<Expr> args = Arrays.asList(new Expr[] { closure, pString });
+        List<Expr> args = Arrays.asList(new Expr[] { closure });
         List<Type> mArgs = Arrays.asList(new Type[] {
-            closure.closureDef().asType(), xts.String()
+            closure.closureDef().asType()
         });
         // TODO: merge with the call() function
         MethodInstance implMI = xts.findMethod(xts.Runtime(),
@@ -307,8 +303,8 @@ public class Desugarer extends ContextVisitor {
     private Stmt visitAsync(Node old, Async a) throws SemanticException {
         Position pos = a.position();
         if (old instanceof Async && ((Async) old).place() instanceof Here)
-            return async(pos, a.body(), a.clocks(), "async-");
-        return async(pos, a.body(), a.clocks(), a.place(), "async-");
+            return async(pos, a.body(), a.clocks());
+        return async(pos, a.body(), a.clocks(), a.place());
     }
 
     private Expr visitHere(Here h) throws SemanticException {
@@ -388,7 +384,7 @@ public class Desugarer extends ContextVisitor {
         Position pos = f.position();
         // Have to desugar some newly-created nodes
         Expr here = visitHere(xnf.Here(f.body().position()));
-        Stmt body = async(f.body().position(), f.body(), f.clocks(), here, "foreach-");
+        Stmt body = async(f.body().position(), f.body(), f.clocks(), here);
         X10Formal formal = (X10Formal) f.formal();
         return xnf.ForLoop(pos, formal, f.domain(), body).locals(formal.explode(this));
     }
@@ -434,7 +430,7 @@ public class Desugarer extends ContextVisitor {
                 xnf.Local(pos, xnf.Id(pos, tmp)).localInstance(lDef.asInstance()).type(dType),
                 xnf.Id(bpos, APPLY),
                 index).methodInstance(mi).type(xts.Place());
-        Stmt body = async(bpos, a.body(), a.clocks(), place, "ateach-");
+        Stmt body = async(bpos, a.body(), a.clocks(), place);
         return xnf.Block(pos,
                 local,
                 xnf.ForLoop(pos, formal,

x10.runtime.17/src-x10/x10/runtime/Activity.x10
@@ -13,7 +13,7 @@ import x10.util.Stack;
 /**
  * @author tardieu
  */
-class Activity(finishState:FinishState, name:String, safe:Boolean) {
+class Activity(finishState:FinishState, safe:Boolean) {
 	// the finish state governing the execution of this activity
 
 	/**
@@ -36,16 +36,16 @@ class Activity(finishState:FinishState, name:String, safe:Boolean) {
 	/**
 	 * Create activity.
 	 */
-	def this(body:()=>Void, finishState:FinishState, name:String, safe:Boolean) {
-		property(finishState, name, safe);
+	def this(body:()=>Void, finishState:FinishState, safe:Boolean) {
+		property(finishState, safe);
 	    this.body = body;
 	}
 
 	/**
 	 * Create clocked activity.
 	 */
-	def this(body:()=>Void, finishState:FinishState, clocks:ValRail[Clock], phases:ValRail[Int], name:String) {
-		this(body, finishState, name, false);
+	def this(body:()=>Void, finishState:FinishState, clocks:ValRail[Clock], phases:ValRail[Int]) {
+		this(body, finishState, false);
 	    clockPhases = new ClockPhases();
 		clockPhases.register(clocks, phases);
 	}
@@ -95,7 +95,8 @@ class Activity(finishState:FinishState, name:String, safe:Boolean) {
 		if (null != clockPhases) clockPhases.drop();
 	}
 
-	public def toString():String = name; 
+    // [DC] The correct thing to do here is do toString() on the closure
+	//public def toString():String = name; 
 }
 
 // vim:shiftwidth=4:tabstop=4:expandtab

x10.runtime.17/src-x10/x10/runtime/Future_c.x10
@@ -14,7 +14,7 @@ import x10.util.GrowableRail;
  * The representation of an X10 future expression.
  * @author tardieu
  */
-public value Future_c[T](name:String) extends Future[T] {
+public value Future_c[T] extends Future[T] {
     /**
      * CountDownLatch for signaling and wait -- can be replaced by a boolean latch
      */
@@ -30,8 +30,7 @@ public value Future_c[T](name:String) extends Future[T] {
 	
 	private val eval:()=>T;
 	
-	public def this(eval:()=>T, name:String) {
-		property(name);
+	public def this(eval:()=>T) {
 		this.eval = eval;
 		result = new GrowableRail[T]();
 	}
@@ -65,5 +64,6 @@ public value Future_c[T](name:String) extends Future[T] {
         }
 	}
 	
-	public def toString():String = name; 
+    // [DC] The correct thing to do here is pull the name from the closure
+	//public def toString():String = name; 
 }

x10.runtime.17/src-x10/x10/runtime/Runtime.x10
@@ -71,7 +71,7 @@ public value Runtime {
 // temporary: printStackTrace call moved to Main template (native code) 
 		try {
 			if (master.loc() == 0) {
-				pool.execute(new Activity(body, rootFinish, "root", true));
+				pool.execute(new Activity(body, rootFinish, true));
 				while (pool.worker().loop(rootFinish, true));
 				if (!NativeRuntime.local(Place.MAX_PLACES - 1)) {
 					val c = ()=>Runtime.quit();
@@ -104,41 +104,41 @@ public value Runtime {
 	/**
 	 * Run async
 	 */
-	public static def runAsync(place:Place, clocks:ValRail[Clock], body:()=>Void, name:String):Void {
+	public static def runAsync(place:Place, clocks:ValRail[Clock], body:()=>Void):Void {
 		val state = currentState();
 		val phases = Rail.makeVal[Int](clocks.length, (i:Nat)=>(clocks(i) as Clock_c).register_c());
 		state.notifySubActivitySpawn();
 		if (place.id == Thread.currentThread().loc()) {
-			pool.execute(new Activity(body, state, clocks, phases, name));
+			pool.execute(new Activity(body, state, clocks, phases));
 		} else {
-            val c = ()=>pool.execute(new Activity(body, state, clocks, phases, name));
+            val c = ()=>pool.execute(new Activity(body, state, clocks, phases));
 			NativeRuntime.runAt(place.id, c);
 		}
 	}
 
-	public static def runAsync(place:Place, body:()=>Void, name:String):Void {
+	public static def runAsync(place:Place, body:()=>Void):Void {
 		val state = currentState();
 		state.notifySubActivitySpawn();
 		val ok = safe();
 		if (place.id == Thread.currentThread().loc()) {
-			pool.execute(new Activity(body, state, name, ok));
+			pool.execute(new Activity(body, state, ok));
 		} else {
-            val c = ()=>pool.execute(new Activity(body, state, name, ok));
+            val c = ()=>pool.execute(new Activity(body, state, ok));
 			NativeRuntime.runAt(place.id, c);
 		}
 	}
 
-	public static def runAsync(clocks:ValRail[Clock], body:()=>Void, name:String):Void {
+	public static def runAsync(clocks:ValRail[Clock], body:()=>Void):Void {
 		val state = currentState();
 		val phases = Rail.makeVal[Int](clocks.length, (i:Nat)=>(clocks(i) as Clock_c).register_c());
 		state.notifySubActivitySpawn();
-		pool.execute(new Activity(body, state, clocks, phases, name));
+		pool.execute(new Activity(body, state, clocks, phases));
 	}
 
-	public static def runAsync(body:()=>Void, name:String):Void {
+	public static def runAsync(body:()=>Void):Void {
 		val state = currentState();
 		state.notifySubActivitySpawn();
-		pool.execute(new Activity(body, state, name, safe()));
+		pool.execute(new Activity(body, state, safe()));
 	}
 	
 	/**
@@ -164,8 +164,8 @@ public value Runtime {
 	/**
 	 * Eval future expression
 	 */
-	public static def evalFuture[T](place:Place, eval:()=>T, name:String):Future[T] {
-		val futur = at (place) new Future_c[T](eval, name);
+	public static def evalFuture[T](place:Place, eval:()=>T):Future[T] {
+		val futur = at (place) new Future_c[T](eval);
 		async (place) futur.run();
 		return futur;
 	}

commit 2a81eaf21f8bfaeab11d2ccdad1eafc00c1ef51c

Fix desugarer error introduced by r10695.

x10.compiler.p3/src/polyglot/ext/x10/visit/Desugarer.java
@@ -164,14 +164,14 @@ public class Desugarer extends ContextVisitor {
     }
 
     private Expr visitFuture(Future f) throws SemanticException {
-        return visitRemoteClosure(f, EVAL_FUTURE, f.place(), true);
+        return visitRemoteClosure(f, EVAL_FUTURE, f.place());
     }
 
     private Expr visitAtExpr(AtExpr e) throws SemanticException {
-        return visitRemoteClosure(e, EVAL_AT, e.place(), false);
+        return visitRemoteClosure(e, EVAL_AT, e.place());
     }
 
-    private Expr visitRemoteClosure(Closure c, Name implName, Expr place, boolean named) throws SemanticException {
+    private Expr visitRemoteClosure(Closure c, Name implName, Expr place) throws SemanticException {
         Position pos = c.position();
         List<TypeNode> typeArgs = Arrays.asList(new TypeNode[] { c.returnType() });
         ClosureDef fDef = c.closureDef();
@@ -187,10 +187,6 @@ public class Desugarer extends ContextVisitor {
         List<Type> mArgs = new ArrayList<Type>(Arrays.asList(new Type[] {
             xts.Place(), closure.closureDef().asType()
         }));
-        if (named) {
-            args.add(xnf.StringLit(pos, pos.nameAndLineString()).type(xts.String()));
-            mArgs.add(xts.String());
-        }
         List<Type> tArgs = Arrays.asList(new Type[] { fDef.returnType().get() });
         // TODO: merge with the call() function
         MethodInstance implMI = xts.findMethod(xts.Runtime(),