sirinath/x10

commit 71104077636b3912370cc98ba719869f81e4bca7

switched to using c++ initialization lists for closure constructors prerequisite for enabling ref closure formals
git-svn-id: svn://svn.code.sf.net/p/x10/code@15527 a326200e-df2c-42e0-81fa-597e909af41d

trunk/x10.compiler/src/x10cpp/visit/MessagePassingCodeGenerator.java
@@ -4077,8 +4077,8 @@ public class MessagePassingCodeGenerator extends X10DelegatingVisitor {
 
         generateClosureSerializationFunctions(c, cnamet, inc, n.body());
 
-        inc.write(cname+"("+SERIALIZATION_MARKER+") { }");
-        inc.newline(); inc.forceNewline();
+//        inc.write(cname+"("+SERIALIZATION_MARKER+") { }");
+//        inc.newline(); inc.forceNewline();
 
         inc.write(cname+"(");
         for (int i = 0; i < c.variables.size(); i++) {
@@ -4090,8 +4090,8 @@ public class MessagePassingCodeGenerator extends X10DelegatingVisitor {
             else name = mangled_non_method_name(name);
             inc.write(Emitter.translateType(var.type(), true) + " " + name);
         }
-        inc.write(") {");
-        inc.newline(4); inc.begin(0);
+        inc.write(")");
+        inc.begin(0);
         // FIXME: factor out this loop
         for (int i = 0 ; i < c.variables.size() ; i++) {
             VarInstance var = (VarInstance) c.variables.get(i);
@@ -4099,11 +4099,11 @@ public class MessagePassingCodeGenerator extends X10DelegatingVisitor {
             if (name.equals(THIS))
                 name = SAVED_THIS;
             else name = mangled_non_method_name(name);
-            if (i > 0) inc.newline();
-            inc.write("this->" + name + " = " + name + ";");
+            if (i > 0) inc.write(", "); else inc.write(" : ");
+            inc.write(name + "(" + name + ")");
         }
-        inc.end(); inc.newline();
-        inc.write("}"); inc.newline(); inc.forceNewline();
+        inc.end();
+        inc.write(" { }"); inc.newline(); inc.forceNewline();
 
         inc.write("static const x10aux::serialization_id_t "+SERIALIZATION_ID_FIELD+";");
         inc.newline(); inc.forceNewline();
@@ -4217,11 +4217,20 @@ public class MessagePassingCodeGenerator extends X10DelegatingVisitor {
 
         inc.write("template<class __T> static "+make_ref("__T")+" "+DESERIALIZE_METHOD+"("+DESERIALIZATION_BUFFER+" &buf) {");
         inc.newline(4); inc.begin(0);
+        // FIXME: factor out this loop
+        for (int i = 0; i < c.variables.size(); i++) {
+            VarInstance var = (VarInstance) c.variables.get(i);
+            Type t = var.type();
+            String type = Emitter.translateType(t, true);
+            String name = var.name().toString();
+            if (name.equals(THIS))
+                name = SAVED_THIS;
+            else name = mangled_non_method_name(name);
+            inc.write(type + " that_"+name+" = buf.read"+chevrons(Emitter.translateType(var.type(), true))+"();");
+            inc.newline();
+        }
         inc.write(make_ref(cnamet)+" this_ = new (x10aux::alloc"+chevrons(cnamet)+"()) "+
-                  cnamet+"("+SERIALIZATION_MARKER+"());");
-        inc.newline();
-        inc.write("buf.record_reference(this_); // TODO: avoid; closure");
-        inc.newline();
+                  cnamet+"(");
         // FIXME: factor out this loop
         for (int i = 0; i < c.variables.size(); i++) {
             VarInstance var = (VarInstance) c.variables.get(i);
@@ -4229,9 +4238,13 @@ public class MessagePassingCodeGenerator extends X10DelegatingVisitor {
             if (name.equals(THIS))
                 name = SAVED_THIS;
             else name = mangled_non_method_name(name);
-            inc.write("this_->"+name+" = buf.read"+chevrons(Emitter.translateType(var.type(), true))+"();");
-            inc.newline();
+            if (i > 0) inc.write(", ");
+            inc.write("that_"+name);
         }
+        inc.write(");");
+        inc.newline();
+        inc.write("buf.record_reference(this_); // TODO: avoid; closure");
+        inc.newline();
         inc.write("return this_;"); inc.end(); inc.newline();
         inc.write("}"); inc.newline(); inc.forceNewline();
     }

commit 0934eb645eb4284512cc7443ffc471d26f5f6514

Fix closure serialization broken in r15527.
It is critically important that buf.record_reference(obj) is
called BEFORE the body of the closure is deserialized.  If this
is not done and a value captured by the closure (transitively)
refers to an object that has already been deserailized, then the
relative encoding of the previous object will be off by one.

This in turn will result in improper (and not type safe) assignment
of points, which with moderate probability will cause memory 
corruption, crashes, or other bizarre behavior.

I believe this fix will clear up a number of test case regressions
in the C++ backend that happened in the week or so period in mid-August
when regression test machines were done.  It fixed HeatTransfer_v4
and HeatTransfer_v5 (tested and verified), so there is reason to 
believe it will fix other things as well.

trunk/x10.compiler/src/x10cpp/visit/MessagePassingCodeGenerator.java
@@ -4250,6 +4250,9 @@ public class MessagePassingCodeGenerator extends X10DelegatingVisitor {
 
         inc.write("template<class __T> static "+make_ref("__T")+" "+DESERIALIZE_METHOD+"("+DESERIALIZATION_BUFFER+" &buf) {");
         inc.newline(4); inc.begin(0);
+        inc.writeln(cnamet+"* storage = x10aux::alloc"+chevrons(cnamet)+"();");
+        inc.writeln("buf.record_reference("+make_ref(cnamet)+"(storage));");
+        
         // FIXME: factor out this loop
         for (int i = 0; i < c.variables.size(); i++) {
             VarInstance<?> var = (VarInstance<?>) c.variables.get(i);
@@ -4262,8 +4265,7 @@ public class MessagePassingCodeGenerator extends X10DelegatingVisitor {
             inc.write(type + " that_"+name+" = buf.read"+chevrons(Emitter.translateType(var.type(), true))+"();");
             inc.newline();
         }
-        inc.write(make_ref(cnamet)+" this_ = new (x10aux::alloc"+chevrons(cnamet)+"()) "+
-                  cnamet+"(");
+        inc.write(make_ref(cnamet)+" this_ = new (storage) "+cnamet+"(");
         // FIXME: factor out this loop
         for (int i = 0; i < c.variables.size(); i++) {
             VarInstance<?> var = (VarInstance<?>) c.variables.get(i);
@@ -4275,8 +4277,6 @@ public class MessagePassingCodeGenerator extends X10DelegatingVisitor {
             inc.write("that_"+name);
         }
         inc.write(");");
-        inc.newline();
-        inc.write("buf.record_reference(this_); // TODO: avoid; closure");
         inc.newline();
         inc.write("return this_;"); inc.end(); inc.newline();
         inc.write("}"); inc.newline(); inc.forceNewline();