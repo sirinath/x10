sirinath/x10

commit 850c8aa4266512dc5f33c754d1fab6a7a20fcb54

Allow X10ClassType.typeArguments() to return null instead of the earlier copy of the def's typeParameters(). Fix uses of typeArguments() vs. typeParameters(). Fix behavior of inner classes in generic types.
git-svn-id: svn://svn.code.sf.net/p/x10/code/trunk@17969 a326200e-df2c-42e0-81fa-597e909af41d

x10.compiler/src/x10/util/Synthesizer.java
@@ -1352,7 +1352,7 @@ public class Synthesizer {
 		if (base instanceof X10ClassType) {
 			X10ClassType xc = (X10ClassType) base;
 			types = xc.typeArguments();
-			if (! types.isEmpty()) {
+			if (types != null && ! types.isEmpty()) {
 				typeName = xc.def().toString();
 				typeArgs = new ArrayList<TypeNode>(types.size());
 			}

x10.compiler/src/x10/visit/X10LocalClassRemover.java
@@ -85,7 +85,7 @@ public class X10LocalClassRemover extends LocalClassRemover {
                     X10ClassType type = (X10ClassType) X10TypeMixin.baseType(neu.objectType().type());
                     List<Type> ta = new ArrayList<Type>(type.typeArguments());
                     List<ParameterType> params = type.x10Def().typeParameters();
-                    if (!params.isEmpty() && (ta.equals(params) || ta.size() != params.size())) {
+                    if (!params.isEmpty() && (ta == null || ta.size() != params.size())) {
                         assert (context().currentCode() instanceof X10MethodDef);
                         X10MethodDef md = (X10MethodDef) context().currentCode();
                         if (ta.equals(params) && !md.typeParameters().isEmpty())
@@ -129,12 +129,12 @@ public class X10LocalClassRemover extends LocalClassRemover {
             if (!rt.isClass())
                 return decl;
             X10ClassType type = (X10ClassType) X10TypeMixin.baseType(rt.toClass());
-            List<Type> ta = new ArrayList<Type>(type.typeArguments());
+            List<Type> ta = type.typeArguments();
             List<ParameterType> params = type.x10Def().typeParameters();
-            if (!params.isEmpty() && (ta.equals(params) || ta.size() != params.size())) {
+            if (!params.isEmpty() && (ta == null || ta.size() != params.size())) {
                 X10MethodDef md = decl.methodDef();
-                if (ta.equals(params) && !md.typeParameters().isEmpty())
-                    ta.clear();
+                if ((ta == null || ta.equals(params)) && !md.typeParameters().isEmpty())
+                    ta = new ArrayList<Type>();
                 ta.addAll(md.typeParameters());
                 assert (ta.size() == params.size());
             }

x10.compiler/src/x10/ast/X10CanonicalTypeNode_c.java
@@ -55,7 +55,7 @@ import x10.types.X10ClassDef;
 import x10.types.X10ClassType;
 import polyglot.types.Context;
 import x10.types.X10Flags;
-import x10.types.X10ParsedClassType_c;
+import x10.types.X10ParsedClassType;
 import x10.types.XTypeTranslator;
 
 import x10.types.X10TypeMixin;
@@ -207,7 +207,7 @@ public class X10CanonicalTypeNode_c extends CanonicalTypeNode_c implements X10Ca
 	    X10ClassType ct = (X10ClassType) t;
         X10ClassDef def = ct.x10Def();
         final List<Type> typeArgs = ct.typeArguments();
-        final int typeArgNum = typeArgs.size();
+        final int typeArgNum = typeArgs == null ? 0 : typeArgs.size();
         final List<ParameterType> typeParam = def.typeParameters();
         final int typeParamNum = typeParam.size();
 
@@ -216,10 +216,7 @@ public class X10CanonicalTypeNode_c extends CanonicalTypeNode_c implements X10Ca
         // But that is not true for a static method, e.g., Array.make(...)
         // so instead we do this check in all other places (e.g., field access, method definitions, new calls, etc)
         // But I can check it if there are typeArguments.
-
-        // typeArgNum>0 is wrong, cause by default we get typeArgs from our def, so that condition is always true
-        // Instead I use: typeParamNum!=typeArgNum
-        if (typeParamNum!=typeArgNum) X10TypeMixin.checkMissingParameters(t,pos);
+        if (typeArgNum > 0) X10TypeMixin.checkMissingParameters(t,pos);
         
 	    for (int j = 0; j < typeArgNum; j++) {
 	        Type actualType = typeArgs.get(j);
@@ -292,9 +289,9 @@ public class X10CanonicalTypeNode_c extends CanonicalTypeNode_c implements X10Ca
             w.write("<unknown-type>");
         } else {
             type.get().print(w);
-            if (extras && X10TypeMixin.baseType(type.get()) instanceof X10ParsedClassType_c
+            if (extras && X10TypeMixin.baseType(type.get()) instanceof X10ParsedClassType
                     && !(X10TypeMixin.baseType(type.get()) instanceof ClosureType_c)) {
-                List<Type> typeArguments = ((X10ParsedClassType_c) X10TypeMixin.baseType(type.get())).typeArguments();
+                List<Type> typeArguments = ((X10ParsedClassType) X10TypeMixin.baseType(type.get())).typeArguments();
                 if (typeArguments.size() > 0) {
                     w.write("[");
                     w.allowBreak(2, 2, "", 0); // miser mode

x10.compiler/src/x10/ast/X10ClassDecl_c.java
@@ -966,7 +966,10 @@ public class X10ClassDecl_c extends ClassDecl_c implements X10ClassDecl {
         X10ClassType resultType = (X10ClassType) thisType.asType();
         // for Generic classes
         final List<ParameterType> typeParams = thisType.typeParameters();
-        resultType = (X10ClassType)resultType.typeArguments((List)typeParams);
+        if (!typeParams.isEmpty()) {
+            List<Type> typeArgs = new ArrayList<Type>(typeParams);
+            resultType = (X10ClassType) resultType.typeArguments(typeArgs);
+        }
         X10CanonicalTypeNode returnType = (X10CanonicalTypeNode) xnf.CanonicalTypeNode(pos, resultType);
 
         ConstructorDecl cd = xnf.X10ConstructorDecl(pos,

x10.compiler/src/x10/ast/X10ConstructorDecl_c.java
@@ -67,6 +67,7 @@ import x10.types.X10ConstructorDef;
 import polyglot.types.Context;
 import x10.types.X10Flags;
 import x10.types.X10MemberDef;
+import x10.types.X10ParsedClassType;
 import x10.types.X10ProcedureDef;
 
 import x10.types.X10TypeMixin;
@@ -216,8 +217,11 @@ public class X10ConstructorDecl_c extends ConstructorDecl_c implements X10Constr
         TypeNode htn = (TypeNode) n.visitChild(n.hasType, tb);
         n = (X10ConstructorDecl_c) n.hasType(htn);
         
-        if (returnType == null)
-        	n = (X10ConstructorDecl_c) n.returnType(nf.CanonicalTypeNode(n.position(), currentClass.asType()));
+        if (returnType == null) {
+            X10ParsedClassType rType = (X10ParsedClassType) currentClass.asType();
+            rType = rType.instantiateTypeParametersExplicitly();
+            n = (X10ConstructorDecl_c) n.returnType(nf.CanonicalTypeNode(n.position(), rType));
+        }
         
         ci.setReturnType((Ref<? extends X10ClassType>) n.returnType().typeRef());
         
@@ -528,10 +532,12 @@ public class X10ConstructorDecl_c extends ConstructorDecl_c implements X10Constr
         
         Type retTypeBase =  n.returnType().type();
         retTypeBase = X10TypeMixin.baseType(retTypeBase);
+        retTypeBase = X10TypeMixin.instantiateTypeParametersExplicitly(retTypeBase);
         
         X10ConstructorDef nnci = (X10ConstructorDef) n.constructorDef();
         // Type clazz = ((X10Type) nnci.asInstance().container()).setFlags(X10Flags.ROOTED);
         Type clazz = nnci.asInstance().container();
+        clazz = X10TypeMixin.instantiateTypeParametersExplicitly(clazz);
         if (! ts.typeEquals(retTypeBase, clazz, tc.context())) {
             Errors.issue(tc.job(),
                     new SemanticException("The return type of the constructor (" + retTypeBase + ") must be derived from the type of the class (" + clazz + ") on which the constructor is defined.",    n.position()));

x10.compiler/src/x10/ast/X10New_c.java
@@ -449,7 +449,7 @@ public class X10New_c extends New_c implements X10New {
         }
 
         X10ParsedClassType container = (X10ParsedClassType) ci.container();
-        if (!ct.typeArguments().isEmpty() && ct.typeArguments().equals(container.x10Def().typeParameters())) {
+        if (!ct.x10Def().typeParameters().isEmpty() && (ct.typeArguments() == null || ct.typeArguments().isEmpty())) {
             t = new TypeParamSubst(xts, container.typeArguments(), container.x10Def().typeParameters()).reinstantiate(t);
             result = (X10New_c) result.objectType(result.objectType().typeRef(Types.ref(t)));
         }
@@ -457,9 +457,10 @@ public class X10New_c extends New_c implements X10New {
         TypeSystem ts = (TypeSystem) tc.typeSystem();
         Type tp = ci.returnType();
         tp = PlaceChecker.ReplaceHereByPlaceTerm(tp, (Context) tc.context());
-        Type tp1 = (Type) tp.copy();
+        Type tp1 = X10TypeMixin.instantiateTypeParametersExplicitly(tp);
+        Type t1 = X10TypeMixin.instantiateTypeParametersExplicitly(t);
         
-        if (!ts.isSubtype(tp1, t, tc.context())) {
+        if (!ts.isSubtype(tp1, t1, tc.context())) {
             throw new SemanticException("Constructor return type " + tp + " is not a subtype of " + t + ".", result.position());
         }
         if (ts.hasUnknown(tp1)) {

x10.compiler/src/x10/emitter/Emitter.java
@@ -421,8 +421,8 @@ public class Emitter {
 			X10ClassDef cd = ((X10ClassType) type).x10Def();
 			String pat = getJavaRep(cd, boxPrimitives);
 			if (pat != null) {
-				List<Type> typeArguments = ((X10ClassType) type)
-						.typeArguments();
+				List<Type> typeArguments = ((X10ClassType) type).typeArguments();
+				if (typeArguments == null) typeArguments = Collections.<Type>emptyList();
 				Object[] o = new Object[typeArguments.size() + 1];
 				int i = 0;
 				NodeFactory nf = tr.nodeFactory();
@@ -566,12 +566,14 @@ public class Emitter {
 		if (printTypeParams) {
 			if (type instanceof X10ClassType) {
 				X10ClassType ct = (X10ClassType) type;
+				List<Type> typeArgs = ct.typeArguments();
+				if (typeArgs == null) typeArgs = new ArrayList<Type>(ct.x10Def().typeParameters());
 				String sep = "<";
-				for (int i = 0; i < ct.typeArguments().size(); i++) {
+				for (int i = 0; i < typeArgs.size(); i++) {
 					w.write(sep);
 					sep = ", ";
 
-					Type a = ct.typeArguments().get(i);
+					Type a = typeArgs.get(i);
 
 					final boolean variance = false;
 					if (!inSuper && variance) {
@@ -594,7 +596,7 @@ public class Emitter {
 									: 0)
 									| X10PrettyPrinterVisitor.BOX_PRIMITIVES);
 				}
-				if (ct.typeArguments().size() > 0)
+				if (typeArgs.size() > 0)
 					w.write(">");
 			}
 		}
@@ -1244,7 +1246,7 @@ public class Emitter {
         Type t = X10TypeMixin.baseType(type);
         if (t instanceof X10ClassType && (printIncludingGeneric || (!printIncludingGeneric && !containsTypeParam(t)))) {
             X10ClassType x10t = (X10ClassType) t;
-            if (x10t.typeArguments().size() > 0) {
+            if (x10t.typeArguments() != null && x10t.typeArguments().size() > 0) {
                 sb.append("_");
                 sb.append(i);
                 sb.append("_");
@@ -1273,7 +1275,7 @@ public class Emitter {
         if (t instanceof X10ClassType) {
             X10ClassType x10t = (X10ClassType) t;
             sb.append(x10t.fullName().toString().replace(".", "$"));
-            if (x10t.typeArguments().size() > 0) {
+            if (x10t.typeArguments() != null && x10t.typeArguments().size() > 0) {
                 List<Type> ts = x10t.typeArguments();
                 for (Type t1 : ts) {
                     appendParameterizedType(ct, sb, X10TypeMixin.baseType(t1));
@@ -1884,9 +1886,10 @@ public class Emitter {
 			rttShortName = new Join(this, "", rttShortName(def), "");
 		} else {
 			X10ClassType ct = (X10ClassType) def.asType();
-			rttShortName = new Join(this, ", ", ct.typeArguments());
 			List<TypeExpander> args = new ArrayList<TypeExpander>();
-			for (Type a : ct.typeArguments()) {
+			List<Type> typeArgs = ct.typeArguments();
+			if (typeArgs == null) typeArgs = Collections.<Type>emptyList();
+			for (Type a : typeArgs) {
 				args.add(new TypeExpander(this, a,
 						X10PrettyPrinterVisitor.PRINT_TYPE_PARAMS
 								| X10PrettyPrinterVisitor.BOX_PRIMITIVES));
@@ -2182,8 +2185,11 @@ public class Emitter {
 		}
 		
 		if (exprTgtType != null) {
-			if (exprTgtType instanceof X10ParsedClassType_c && !((X10ParsedClassType_c) exprTgtType).typeArguments().isEmpty()) {
-				return false;
+			if (exprTgtType instanceof X10ParsedClassType_c) {
+				List<Type> typeArguments = ((X10ParsedClassType_c) exprTgtType).typeArguments();
+				if (typeArguments != null && !typeArguments.isEmpty()) {
+					return false;
+				}
 			}
 		}
 		return true;
@@ -2524,7 +2530,7 @@ public class Emitter {
         }
         if (type instanceof X10ClassType) {
             X10ClassType x10Type = (X10ClassType) type;
-            if (x10Type.typeArguments().size() > 0) {
+            if (x10Type.typeArguments() != null && x10Type.typeArguments().size() > 0) {
                 for (int i = 0; i < x10Type.typeArguments().size(); i++) {
                     if (i == 0) { 
                         w.write("new x10.rtt.ParameterizedType(");

x10.compiler/src/x10/emitter/RuntimeTypeExpander.java
@@ -11,6 +11,7 @@
 
 package x10.emitter;
 
+import java.util.Collections;
 import java.util.List;
 
 import polyglot.types.Type;
@@ -102,26 +103,28 @@ final public class RuntimeTypeExpander extends Expander {
             	return;
             }
             
+            List<Type> typeArgs = ct.typeArguments();
+            if (typeArgs == null) typeArgs = Collections.<Type>emptyList();
             if (pat == null) {
                 // XTENLANG-1102
-                if (ct.isGloballyAccessible() && ct.typeArguments().size() == 0) {
+                if (ct.isGloballyAccessible() && typeArgs.size() == 0) {
                     er.w.write(cd.fullName().toString() + "." + "_RTT");
                 } else {
                     er.w.write("new x10.rtt.ParameterizedType(");
                     er.w.write(cd.fullName().toString() + "." + "_RTT");
-                    for (int i = 0; i < ct.typeArguments().size(); i++) {
+                    for (int i = 0; i < typeArgs.size(); i++) {
                         er.w.write(", ");
-                        new RuntimeTypeExpander(er, ct.typeArguments().get(i)).expand(tr);
+                        new RuntimeTypeExpander(er, typeArgs.get(i)).expand(tr);
                     }
                     er.w.write(")");
                 }
                 return;
             }
             else {
-            	Object[] components = new Object[1 + ct.typeArguments().size() * 2];
+            	Object[] components = new Object[1 + typeArgs.size() * 2];
             	int i = 0;
             	components[i++] = new TypeExpander(er, ct, X10PrettyPrinterVisitor.PRINT_TYPE_PARAMS | X10PrettyPrinterVisitor.BOX_PRIMITIVES);
-            	for (Type at : ct.typeArguments()) {
+            	for (Type at : typeArgs) {
             		components[i++] = new TypeExpander(er, at, X10PrettyPrinterVisitor.PRINT_TYPE_PARAMS | X10PrettyPrinterVisitor.BOX_PRIMITIVES);
             		components[i++] = new RuntimeTypeExpander(er, at);
             	}

commit c72ca401cd5029338a7967fd7a2acbb54a4a8682

Fix minor regressions introduced by r17969.

x10.compiler/src/x10/util/Synthesizer.java
@@ -1352,7 +1352,9 @@ public class Synthesizer {
 		if (base instanceof X10ClassType) {
 			X10ClassType xc = (X10ClassType) base;
 			types = xc.typeArguments();
-			if (types != null && ! types.isEmpty()) {
+			if (types == null)
+			    types = Collections.<Type>emptyList();
+			if (! types.isEmpty()) {
 				typeName = xc.def().toString();
 				typeArgs = new ArrayList<TypeNode>(types.size());
 			}

x10.compiler/src/x10/visit/X10LocalClassRemover.java
@@ -83,13 +83,16 @@ public class X10LocalClassRemover extends LocalClassRemover {
                 
                 if (container.def() == theLocalClass) {
                     X10ClassType type = (X10ClassType) X10TypeMixin.baseType(neu.objectType().type());
-                    List<Type> ta = new ArrayList<Type>(type.typeArguments());
+                    List<Type> ta = type.typeArguments();
                     List<ParameterType> params = type.x10Def().typeParameters();
                     if (!params.isEmpty() && (ta == null || ta.size() != params.size())) {
                         assert (context().currentCode() instanceof X10MethodDef);
                         X10MethodDef md = (X10MethodDef) context().currentCode();
-                        if (ta.equals(params) && !md.typeParameters().isEmpty())
-                            ta.clear();
+                        if (ta == null) {
+                            ta = new ArrayList<Type>();
+                        } else if (!md.typeParameters().isEmpty()) {
+                            ta = new ArrayList<Type>(ta);
+                        }
                         ta.addAll(md.typeParameters());
                         assert (ta.size() == params.size());
                     }

x10.compiler/src/x10c/visit/VarsBoxer.java
@@ -418,7 +418,7 @@ public class VarsBoxer extends ContextVisitor {
         typeArgs.add(lilocal.type());
         X10ParsedClassType grt = globalRefType.typeArguments(typeArgs);
         
-        MethodDef md = xts.methodDef(pos, Types.ref(grt), Flags.FINAL, Types.ref(grt.typeArguments().get(0)), mname, argTypes);
+        MethodDef md = xts.methodDef(pos, Types.ref(grt), Flags.FINAL, Types.ref(globalRefType.x10Def().typeParameters().get(0)), mname, argTypes);
         MethodInstance mi = md.asInstance();
         
         Local local = (Local) xnf.Local(pos, xnf.Id(pos, lilocal.name().toString() + POSTFIX_BOXED_VAR)).localInstance(libox).type(libox.type());