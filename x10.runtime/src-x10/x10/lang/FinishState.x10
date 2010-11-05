sirinath/x10

commit fae33d0e851203a875c787f8ab14fe5eec46c7f7

the body of a finish is no longer counted as a separate activity
git-svn-id: svn://svn.code.sf.net/p/x10/code/trunk@17964 a326200e-df2c-42e0-81fa-597e909af41d

x10.runtime/src-x10/x10/lang/FinishState.x10
@@ -29,12 +29,13 @@ abstract class FinishState {
 
     // a finish with local asyncs only
     static class LocalFinish extends FinishState {
-        private val count = new AtomicInteger(1);
-        private val latch = new Latch();
+        private val count = new AtomicInteger(0);
+        private var latch:Latch = null;
         private var exceptions:Stack[Throwable]; // lazily initialized
         private val lock = new Lock();
         public def notifySubActivitySpawn(place:Place) {
             assert place.id == Runtime.hereInt();
+            if (null == latch) latch = new Latch();
             count.getAndIncrement();
         }
         public def notifyActivityCreation() {}
@@ -48,8 +49,10 @@ abstract class FinishState {
             lock.unlock();
         }
         public def waitForFinish(safe:Boolean) {
-            if (!Runtime.NO_STEALS && safe) Runtime.worker().join(latch);
-            latch.await();
+            if (null != latch) {
+                if (!Runtime.NO_STEALS && safe) Runtime.worker().join(latch);
+                latch.await();
+            }
             val t = MultipleExceptions.make(exceptions);
             if (null != t) throw t;
         }
@@ -139,7 +142,7 @@ abstract class FinishState {
             this(new RootFinish(latch));
         }
         def this() {
-            this(new Latch());
+            this(new RootFinish());
         }
         protected def this(ref:GlobalRef[FinishState]) {
             super(ref);
@@ -155,20 +158,20 @@ abstract class FinishState {
     }
 
     static class RootFinish extends RootFinishSkeleton {
-        protected val latch:Latch;
+        protected var latch:Latch;
         protected var exceptions:Stack[Throwable]; // lazily initialized
         protected val counts:Rail[Int] = Rail.make[Int](Place.MAX_PLACES, 0);
         protected val seen:Rail[Boolean] = Rail.make[Boolean](Place.MAX_PLACES, false);
         protected val lock:Lock = new Lock();
         def this(latch:Latch) {
             this.latch = latch;
-            counts(Runtime.hereInt()) = 1;
         }
         def this() {
-            this(new Latch());
+            this.latch = null;
         }
         public def notifySubActivitySpawn(place:Place):void {
             lock.lock();
+            if (null == latch) latch = new Latch();
             counts(place.parent().id)++;
             lock.unlock();
         }
@@ -194,8 +197,10 @@ abstract class FinishState {
             lock.unlock();
         }
         public def waitForFinish(safe:Boolean):void {
-            if (!Runtime.NO_STEALS && safe) Runtime.worker().join(latch);
-            latch.await();
+            if (null != latch) {
+                if (!Runtime.NO_STEALS && safe) Runtime.worker().join(latch);
+                latch.await();
+            }
             val root = ref();
             val closure = ()=>@RemoteInvocation { Runtime.runtime().finishStates.remove(root); };
             seen(Runtime.hereInt()) = false;

x10.runtime/src-x10/x10/lang/Runtime.x10
@@ -512,6 +512,7 @@ import x10.util.Box;
 
             if (hereInt() == 0) {
                 val rootFinish = new FinishState.Finish(runtime().pool.latch);
+                rootFinish.notifySubActivitySpawn(here());
                 // in place 0 schedule the execution of the static initializers fby main activity
                 execute(new Activity(()=>{finish init(); body();}, rootFinish, true));
 
@@ -816,7 +817,6 @@ import x10.util.Box;
     public static def stopFinish(f:FinishState):void {
         val a = activity();
         val finishState = a.swapFinish(f);
-        finishState.notifyActivityTermination();
         finishState.waitForFinish(a.safe());
     }
 
@@ -839,7 +839,6 @@ import x10.util.Box;
 
     public static def stopCollectingFinish[T](f:FinishState):T {
         val state = activity().swapFinish(f);
-        state.notifyActivityTermination();
         return (state as FinishState.CollectingFinish[T]).waitForFinishExpr(true);
     }
 

commit 1710f2e94506d540b8f63aac5f4c6b28bdc02816

fixed finish regression introduced in r17964

x10.runtime/src-x10/x10/lang/FinishState.x10
@@ -29,13 +29,12 @@ abstract class FinishState {
 
     // a finish with local asyncs only
     static class LocalFinish extends FinishState {
-        private val count = new AtomicInteger(0);
-        private var latch:Latch = null;
+        private val count = new AtomicInteger(1);
+        private val latch = new Latch();
         private var exceptions:Stack[Throwable]; // lazily initialized
         private val lock = new Lock();
         public def notifySubActivitySpawn(place:Place) {
             assert place.id == Runtime.hereInt();
-            if (null == latch) latch = new Latch();
             count.getAndIncrement();
         }
         public def notifyActivityCreation() {}
@@ -49,10 +48,9 @@ abstract class FinishState {
             lock.unlock();
         }
         public def waitForFinish(safe:Boolean) {
-            if (null != latch) {
-                if (!Runtime.NO_STEALS && safe) Runtime.worker().join(latch);
-                latch.await();
-            }
+            notifyActivityTermination();
+            if (!Runtime.NO_STEALS && safe) Runtime.worker().join(latch);
+            latch.await();
             val t = MultipleExceptions.make(exceptions);
             if (null != t) throw t;
         }
@@ -320,7 +318,7 @@ abstract class FinishState {
             this(new RootFinish(latch));
         }
         def this() {
-            this(new RootFinish());
+            this(new Latch());
         }
         protected def this(ref:GlobalRef[FinishState]) {
             super(ref);
@@ -336,20 +334,17 @@ abstract class FinishState {
     }
 
     static class RootFinish extends RootFinishSkeleton {
-        protected var latch:Latch;
+        protected val latch:Latch;
         protected var exceptions:Stack[Throwable]; // lazily initialized
         protected val counts:Rail[Int] = Rail.make[Int](Place.MAX_PLACES, 0);
         protected val seen:Rail[Boolean] = Rail.make[Boolean](Place.MAX_PLACES, false);
         protected val lock:Lock = new Lock();
         def this(latch:Latch) {
             this.latch = latch;
-        }
-        def this() {
-            this.latch = null;
+            counts(Runtime.hereInt())++;
         }
         public def notifySubActivitySpawn(place:Place):void {
             lock.lock();
-            if (null == latch) latch = new Latch();
             counts(place.parent().id)++;
             lock.unlock();
         }
@@ -375,10 +370,9 @@ abstract class FinishState {
             lock.unlock();
         }
         public def waitForFinish(safe:Boolean):void {
-            if (null != latch) {
-                if (!Runtime.NO_STEALS && safe) Runtime.worker().join(latch);
-                latch.await();
-            }
+            notifyActivityTermination();
+            if (!Runtime.NO_STEALS && safe) Runtime.worker().join(latch);
+            latch.await();
             val root = ref();
             val closure = ()=>@RemoteInvocation { Runtime.runtime().finishStates.remove(root); };
             seen(Runtime.hereInt()) = false;
@@ -561,7 +555,7 @@ abstract class FinishState {
     static class RootCollectingFinish[T] extends RootFinish implements CollectingFinishState[T] {
         val sr:StatefulReducer[T];
         def this(reducer:Reducible[T]) {
-           super();
+           super(new Latch());
            sr = new StatefulReducer[T](reducer);
         }
         public def accept(t:T, id:Int) {

x10.runtime/src-x10/x10/lang/Runtime.x10
@@ -513,7 +513,6 @@ import x10.util.Box;
 
             if (hereInt() == 0) {
                 val rootFinish = new FinishState.Finish(runtime().pool.latch);
-                rootFinish.notifySubActivitySpawn(here());
                 // in place 0 schedule the execution of the static initializers fby main activity
                 execute(new Activity(()=>{finish init(); body();}, rootFinish, true));
 