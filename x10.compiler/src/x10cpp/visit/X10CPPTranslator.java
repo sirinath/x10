sirinath/x10

commit a52e2f1cbbdc673f814e546a77fafbe5093cf3fe

Factor out main() method detection into HierarchyUtils.isMainMethod(). Instead of generating main stubs for every main() method, record them all and generate one stub in the end (fixes XTENLANG-2094).
git-svn-id: svn://svn.code.sf.net/p/x10/code/trunk@18559 a326200e-df2c-42e0-81fa-597e909af41d

x10.compiler/src/x10cpp/visit/X10CPPTranslator.java
@@ -69,16 +69,19 @@ import polyglot.main.Report;
 import polyglot.types.ClassType;
 import polyglot.types.Context;
 import polyglot.types.MemberDef;
+import polyglot.types.MethodDef;
 import polyglot.types.Name;
 import polyglot.types.Package;
 import polyglot.types.QName;
 import polyglot.types.SemanticException;
 import polyglot.types.Type;
 import polyglot.types.TypeSystem;
+import polyglot.types.Types;
 import polyglot.util.CodeWriter;
 import polyglot.util.ErrorInfo;
 import polyglot.util.ErrorQueue;
 import polyglot.util.InternalCompilerError;
+import polyglot.util.SimpleCodeWriter;
 import polyglot.util.StdErrorQueue;
 import polyglot.util.StringUtil;
 import polyglot.visit.Translator;
@@ -91,7 +94,9 @@ import x10.types.X10TypeSystem_c;
 import x10.util.ClassifiedStream;
 import x10.util.StreamWrapper;
 import x10.util.WriterStreams;
+import x10cpp.Configuration;
 import x10cpp.X10CPPCompilerOptions;
+import x10cpp.X10CPPJobExt;
 import x10cpp.debug.LineNumberMap;
 import x10cpp.postcompiler.AIX_CXXCommandBuilder;
 import x10cpp.postcompiler.CXXCommandBuilder;
@@ -457,8 +462,9 @@ public class X10CPPTranslator extends Translator {
 
 	public static final String postcompile = "postcompile";
 
+	public static final String MAIN_STUB_NAME = "xxx_main_xxx";
 
-    /**
+	/**
 	 * The post-compiler option has the following structure:
 	 * "[pre-command with options (usually g++)] [(#|%) [post-options (usually extra files)] [(#|%) [library options]]]".
 	 * Using '%' instead of '#' to delimit a section will cause the default values in that section to be omitted.
@@ -469,16 +475,75 @@ public class X10CPPTranslator extends Translator {
 
 		if (options.post_compiler != null && !options.output_stdout) {
 			// use set to avoid duplicates
-            Set<String> compilationUnits = new HashSet<String>(options.compilationUnits());
-            CXXCommandBuilder ccb = CXXCommandBuilder.getCXXCommandBuilder(options, eq);
-            String[] cxxCmd = ccb.buildCXXCommandLine(compilationUnits);
+			Set<String> compilationUnits = new HashSet<String>(options.compilationUnits());
+
+			try {
+			    final File file = outputFile(options, null, MAIN_STUB_NAME, "cc");
+			    ExtensionInfo ext = compiler.sourceExtension();
+			    SimpleCodeWriter sw = new SimpleCodeWriter(ext.targetFactory().outputWriter(file),
+			            compiler.outputWidth());
+			    List<MethodDef> mainMethods = new ArrayList<MethodDef>();
+			    for (Job job : ext.scheduler().commandLineJobs()) {
+			        mainMethods.addAll(getMainMethods(job));
+			    }
+			    if (mainMethods.size() < 1) {
+			        // If there are no main() methods in the command-line jobs, try other files
+			        for (Job job : ext.scheduler().jobs()) {
+			            mainMethods.addAll(getMainMethods(job));
+			        }
+			    }
+			    if (mainMethods.size() < 1) {
+			        eq.enqueue(ErrorInfo.SEMANTIC_ERROR, "No main method found");
+			        return false;
+			    } else if (mainMethods.size() > 1) {
+			        eq.enqueue(ErrorInfo.SEMANTIC_ERROR,
+			                "Multiple main() methods found, please specify MAIN_CLASS:"+listMethods(mainMethods));
+			        return false;
+			    }
+			    assert (mainMethods.size() == 1);
+			    X10ClassType container = (X10ClassType) Types.get(mainMethods.get(0).container());
+			    MessagePassingCodeGenerator.processMain(container, sw);
+			    sw.flush();
+			    sw.close();
+			    compilationUnits.add(file.getName());
+			}
+			catch (IOException e) {
+			    eq.enqueue(ErrorInfo.IO_ERROR, "I/O error while translating: " + e.getMessage());
+			    return false;
+			}
+
+			CXXCommandBuilder ccb = CXXCommandBuilder.getCXXCommandBuilder(options, eq);
+			String[] cxxCmd = ccb.buildCXXCommandLine(compilationUnits);
 
 			if (!doPostCompile(options, eq, compilationUnits, cxxCmd)) return false;
-            
+
 			// FIXME: [IP] HACK: Prevent the java post-compiler from running
 			options.post_compiler = null;
 		}
 		return true;
+	}
+
+	private static List<MethodDef> getMainMethods(Job job) {
+	    X10CPPJobExt jobext = (X10CPPJobExt) job.ext();
+	    if (Configuration.MAIN_CLASS != null) {
+	        for (MethodDef md : jobext.mainMethods()) {
+	            Name containerName = ((X10ClassType) Types.get(md.container())).name();
+	            if (containerName.toString().equals(Configuration.MAIN_CLASS)) {
+	                return Collections.singletonList(md);
+	            }
+	        }
+	        return Collections.<MethodDef>emptyList();
+	    } else {
+	        return jobext.mainMethods();
+	    }
+	}
+
+	private static String listMethods(List<MethodDef> mainMethods) {
+	    StringBuilder sb = new StringBuilder();
+	    for (MethodDef md : mainMethods) {
+            sb.append("\n\t").append(md.toString());
+        }
+	    return sb.toString();
 	}
 
     public static boolean doPostCompile(Options options, ErrorQueue eq, Collection<String> outputFiles, String[] cxxCmd) {

x10.compiler/src/x10/compiler/ws/WSCodeGenerator.java
@@ -24,8 +24,6 @@ import polyglot.frontend.Job;
 import polyglot.types.ClassDef;
 import polyglot.types.ClassType;
 import polyglot.types.ConstructorDef;
-import polyglot.types.MethodDef;
-import polyglot.types.ProcedureDef;
 import polyglot.types.SemanticException;
 import polyglot.types.TypeSystem;
 import polyglot.visit.ContextVisitor;
@@ -44,6 +42,7 @@ import x10.compiler.ws.codegen.WSMethodFrameClassGen;
 import x10.compiler.ws.util.WSCallGraph;
 import x10.compiler.ws.util.WSCallGraphNode;
 import x10.types.ClosureDef;
+import x10.types.X10MethodDef;
 import polyglot.types.Context;
 import polyglot.types.TypeSystem;
 import x10.types.checker.PlaceChecker;
@@ -127,9 +126,9 @@ public class WSCodeGenerator extends ContextVisitor {
         }
 
         // transform methods
-        if(n instanceof MethodDecl) {
-            MethodDecl mDecl = (MethodDecl)n;
-            MethodDef mDef = mDecl.methodDef();
+        if(n instanceof X10MethodDecl) {
+            X10MethodDecl mDecl = (X10MethodDecl)n;
+            X10MethodDef mDef = mDecl.methodDef();
             if(wts.isTargetProcedure(mDef)){
                 if(debugLevel > 3){
                     System.out.println("[WS_INFO] Start transforming target method: " + mDef.name());

x10.compiler/src/x10/compiler/ws/codegen/WSMethodFrameClassGen.java
@@ -41,6 +41,7 @@ import x10.types.X10ClassDef;
 import x10.types.X10ClassType;
 import x10.types.X10Flags;
 import x10.types.X10MethodDef;
+import x10.util.HierarchyUtils;
 import x10.util.synthesizer.CodeBlockSynth;
 import x10.util.synthesizer.FieldSynth;
 import x10.util.synthesizer.InstanceCallSynth;
@@ -68,12 +69,12 @@ public class WSMethodFrameClassGen extends WSRegularFrameClassGen {
     
 
     public WSMethodFrameClassGen(Job job, NodeFactory xnf, Context xct,
-                                  MethodDef methodDef, MethodDecl methodDecl, WSTransformState wts) {
+                                  X10MethodDef methodDef, MethodDecl methodDecl, WSTransformState wts) {
         this(job, xnf, xct, methodDef, methodDecl, wts,
-                X10PrettyPrinterVisitor.isMainMethodInstance(methodDef.asInstance(), xct));
+                HierarchyUtils.isMainMethod(methodDef, xct));
     }
     public WSMethodFrameClassGen(Job job, NodeFactory xnf, Context xct,
-                                  MethodDef methodDef, MethodDecl methodDecl, WSTransformState wts,
+                                  X10MethodDef methodDef, MethodDecl methodDecl, WSTransformState wts,
                                   boolean isMain) {
     
         super(job, xnf, xct, wts, WSCodeGenUtility.getMethodBodyClassName(methodDef),

x10.compiler/src/x10/util/HierarchyUtils.java
@@ -16,9 +16,14 @@ import java.util.LinkedHashSet;
 import java.util.Set;
 
 import polyglot.types.ClassType;
+import polyglot.types.Context;
 import polyglot.types.Flags;
 import polyglot.types.MethodInstance;
 import polyglot.types.Type;
+import polyglot.types.TypeSystem;
+import x10.types.X10ClassType;
+import x10.types.X10MethodDef;
+import x10.types.X10MethodInstance;
 
 public class HierarchyUtils {
 
@@ -97,4 +102,23 @@ public class HierarchyUtils {
 
 		return methods;
 	}
+
+	public static boolean isMainMethod(X10MethodDef md, Context context) {
+	    return isMainMethod(md.asInstance(), context);
+	}
+
+	public static boolean isMainMethod(X10MethodInstance mi, Context context) {
+	    final TypeSystem ts = mi.typeSystem();
+	    X10ClassType container = (X10ClassType) mi.container();
+	    boolean result =
+	        mi.flags().isPublic() &&
+	        mi.flags().isStatic() &&
+	        mi.name().toString().equals("main") &&
+	        mi.returnType().isVoid() &&
+	        mi.typeParameters().size() == 0 &&
+	        mi.formalTypes().size() == 1 &&
+	        ts.isSubtype(mi.formalTypes().get(0), ts.Array(ts.String()), context);
+	    return result;
+	}
+
 }

x10.compiler/src/x10/visit/MainMethodFinder.java
@@ -59,6 +59,7 @@ import x10.types.X10ClassType;
 import x10.types.X10Flags;
 import x10.types.X10MethodDef;
 import x10.types.X10TypeMixin;
+import x10.util.HierarchyUtils;
 import polyglot.types.TypeSystem;
 
 /**
@@ -77,13 +78,12 @@ public class MainMethodFinder extends ContextVisitor {
     }
 
     protected Node leaveCall(Node parent, Node old, Node n, NodeVisitor v) throws SemanticException {
-        if (!(n instanceof X10MethodDecl_c))
+        if (!(n instanceof X10MethodDecl))
             return n;
 
-        X10MethodDecl_c m = (X10MethodDecl_c) n;
+        X10MethodDecl m = (X10MethodDecl) n;
 
-        if (m.formals().size() == 1 &&
-                X10PrettyPrinterVisitor.isMainMethod(ts, m.flags().flags(), m.name().id(), m.returnType().type(), m.formals().get(0).declType(), context)) {
+        if (HierarchyUtils.isMainMethod(m.methodDef(), context)) {
             try {
                 hasMain.invoke(null, context.currentClass().name().toString());
             } catch (Throwable t) { t.printStackTrace(); }

x10.compiler/src/x10/visit/X10PrettyPrinterVisitor.java
@@ -176,9 +176,11 @@ import x10.types.X10ClassType;
 import x10.types.X10ConstructorDef;
 import x10.types.X10ConstructorInstance;
 import x10.types.X10FieldInstance;
+import x10.types.X10MethodDef;
 import x10.types.X10MethodInstance;
 import x10.types.X10ParsedClassType_c;
 import x10.types.X10TypeMixin;
+import x10.util.HierarchyUtils;
 import x10c.ast.X10CBackingArrayAccessAssign_c;
 import x10c.ast.X10CBackingArrayAccess_c;
 import x10c.ast.X10CBackingArrayNewArray_c;
@@ -565,7 +567,7 @@ public class X10PrettyPrinterVisitor extends X10DelegatingVisitor {
 
 		Flags flags = n.flags().flags();
 
-		if (n.formals().size() == 1 && isMainMethod(ts, flags, n.name(), n.returnType().type(), n.formals().get(0).declType())){
+		if (isMainMethod(n.methodDef())) {
 			/*Expander throwsClause = new Inline(er, "");
 			if (n.throwTypes().size() > 0) {
 				List<Expander> l = new ArrayList<Expander>();
@@ -600,24 +602,16 @@ public class X10PrettyPrinterVisitor extends X10DelegatingVisitor {
 		er.generateMethodDecl(n, false);
 	}
 
-    private boolean isMainMethod(TypeSystem ts, Flags flags, Id name, Type returnType, Type argType) {
-        return isMainMethod(ts, flags, name.id(), returnType, argType, tr.context());
+    private boolean isMainMethod(X10MethodDef md) {
+        return HierarchyUtils.isMainMethod(md, tr.context());
     }
 
-    public static boolean isMainMethod(TypeSystem ts, Flags flags, Name name, Type returnType, Type argType, Context context) {
-        return name.toString().equals("main") &&
-                flags.isPublic() &&
-                flags.isStatic() &&
-                returnType.isVoid() &&
-                argType.isSubtype(ts.Array(ts.String()), context);
+    private boolean isMainMethod(X10MethodInstance mi) {
+        return HierarchyUtils.isMainMethod(mi, tr.context());
     }
-
-    public static boolean isMainMethodInstance(MethodInstance mi, Context context) {
-        return mi.formalTypes().size() == 1 && isMainMethod((TypeSystem) mi.typeSystem(), mi.flags(), mi.name(), mi.returnType(), mi.formalTypes().get(0), context);
-    }
-
+    
 	public void visit(Id_c n) {
-                w.write(Emitter.mangleToJava(n.id()));
+	    w.write(Emitter.mangleToJava(n.id()));
 	}
 
 
@@ -1741,7 +1735,7 @@ public class X10PrettyPrinterVisitor extends X10DelegatingVisitor {
 	        return;
 	    }
 	    
-	    X10MethodInstance mi = (X10MethodInstance) c.methodInstance();
+	    X10MethodInstance mi = c.methodInstance();
 		Receiver target = c.target();
 
 		// Check for properties accessed using method syntax.  They may have @Native annotations too.
@@ -1810,7 +1804,7 @@ public class X10PrettyPrinterVisitor extends X10DelegatingVisitor {
 		    }
 		}
 		
-		if (isGenericOverloading && mi.formalTypes().size() == 1 && isMainMethod(xts, mi.flags(), c.name(), mi.returnType(), mi.formalTypes().get(0)))
+		if (isGenericOverloading && isMainMethod(mi))
 		{
 		    w.write(Emitter.mangleToJava(c.name().id()));
 		}

x10.compiler/src/x10cpp/ExtensionInfo.java
@@ -25,6 +25,7 @@ import polyglot.frontend.Compiler;
 import polyglot.frontend.Globals;
 import polyglot.frontend.Goal;
 import polyglot.frontend.Job;
+import polyglot.frontend.JobExt;
 import polyglot.frontend.OutputGoal;
 import polyglot.frontend.Scheduler;
 import polyglot.frontend.VisitorGoal;
@@ -121,6 +122,11 @@ public class ExtensionInfo extends x10.ExtensionInfo {
             throw new InternalCompilerError(
                 "Unable to initialize type system: " + e.getMessage(), e);
         }
+    }
+
+    @Override
+    public JobExt jobExt() {
+        return new X10CPPJobExt();
     }
 
     // =================================

x10.compiler/src/x10cpp/X10CPPJobExt.java
@@ -1,0 +1,33 @@
+/*
+ *  This file is part of the X10 project (http://x10-lang.org).
+ *
+ *  This file is licensed to You under the Eclipse Public License (EPL);
+ *  You may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *      http://www.opensource.org/licenses/eclipse-1.0.php
+ *
+ *  (C) Copyright IBM Corporation 2006-2010.
+ */
+
+package x10cpp;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import polyglot.frontend.JobExt;
+import polyglot.types.MethodDef;
+
+public class X10CPPJobExt implements JobExt {
+
+    private List<MethodDef> mainMethods = new ArrayList<MethodDef>();
+
+    public X10CPPJobExt() { }
+
+    public List<MethodDef> mainMethods() {
+        return mainMethods;
+    }
+
+    public void addMainMethod(MethodDef md) {
+        mainMethods.add(md);
+    }
+}

commit 00ab9019155af46f43d38afc0ffb34727a20e1ce

Fix minor regression introduced in r18559.

x10.compiler/src/x10cpp/visit/X10CPPTranslator.java
@@ -70,7 +70,6 @@ import polyglot.types.ClassType;
 import polyglot.types.Context;
 import polyglot.types.MemberDef;
 import polyglot.types.MethodDef;
-import polyglot.types.Name;
 import polyglot.types.Package;
 import polyglot.types.QName;
 import polyglot.types.SemanticException;
@@ -527,7 +526,7 @@ public class X10CPPTranslator extends Translator {
 	    X10CPPJobExt jobext = (X10CPPJobExt) job.ext();
 	    if (Configuration.MAIN_CLASS != null) {
 	        for (MethodDef md : jobext.mainMethods()) {
-	            Name containerName = ((X10ClassType) Types.get(md.container())).name();
+	            QName containerName = ((X10ClassType) Types.get(md.container())).fullName();
 	            if (containerName.toString().equals(Configuration.MAIN_CLASS)) {
 	                return Collections.singletonList(md);
 	            }