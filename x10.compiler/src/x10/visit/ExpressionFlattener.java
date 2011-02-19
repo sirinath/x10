sirinath/x10

commit 0deb2496111bbc073e7637ff7d9b30275c8d768f

wrap inlined code in a labeled block instead of a do
git-svn-id: svn://svn.code.sf.net/p/x10/code/trunk@20247 a326200e-df2c-42e0-81fa-597e909af41d

x10.compiler/src/x10/visit/ExpressionFlattener.java
@@ -249,6 +249,9 @@ public final class ExpressionFlattener extends ContextVisitor {
            loopLabels.setSize(loopLabels.size()-1);
            return returnNode;
         }
+        if (n instanceof Block)
+            return flattenBlock((Block) n, parent instanceof Labeled ?
+                                                labelMap.remove(old) : null);
         if (n instanceof Expr) return flattenExpr((Expr) n);
         if (n instanceof Stmt) return flattenStmt((Stmt) n);
         return n;
@@ -788,7 +791,6 @@ public final class ExpressionFlattener extends ContextVisitor {
     private Stmt flattenStmt(Stmt stmt) {
         if      (stmt instanceof Eval)      return flattenEval((Eval) stmt);
         else if (stmt instanceof LocalDecl) return flattenLocalDecl((LocalDecl) stmt);
-        else if (stmt instanceof Block)     return flattenBlock((Block) stmt);
         else if (stmt instanceof If)        return flattenIf((If) stmt);
         else if (stmt instanceof Return)    return flattenReturn((Return) stmt);
         else if (stmt instanceof Throw)     return flattenThrow((Throw) stmt);
@@ -820,7 +822,9 @@ public final class ExpressionFlattener extends ContextVisitor {
 
     /**
      * Flatten a block.
-     * (Actually, this is a no-op, the only block with a value, StmtExpr, has already been handled.)
+     * If this is a labeled block, we stick the lables on it because flattenLabled
+     * is about to rip them off.
+     * (Otherwise, this is a no-op, the only block with a value, StmtExpr, has already been handled.)
      * <pre>
      * { S; ({s1; e1}) }  ->  { S; s1; e1 } 
      * </pre>
@@ -828,10 +832,13 @@ public final class ExpressionFlattener extends ContextVisitor {
      * @param stmt the block to be flattened
      * @return a block with all of its constituent statements flattened.
      */
-    private Stmt flattenBlock(Block stmt) {
+    private Stmt flattenBlock(Block stmt, List<Labeled> list) {
         assert (!(stmt instanceof StmtExpr));
         // return syn.toStmtSeq(stmt);
-        return stmt;
+
+        // we're about to strip off our parents label in flattenLabled.
+        // So, if this block is labeled we'd better cram it on here.
+        return label(stmt,list);
     }
 
     /**

x10.compiler/src/x10/visit/Inliner.java
@@ -23,6 +23,7 @@ import polyglot.ast.AmbExpr;
 import polyglot.ast.AmbTypeNode;
 import polyglot.ast.Assign;
 import polyglot.ast.Block;
+import polyglot.ast.Branch;
 import polyglot.ast.Call;
 import polyglot.ast.ClassDecl;
 import polyglot.ast.ClassMember;
@@ -31,6 +32,7 @@ import polyglot.ast.Eval;
 import polyglot.ast.Expr;
 import polyglot.ast.Field;
 import polyglot.ast.Formal;
+import polyglot.ast.Id;
 import polyglot.ast.LocalDecl;
 import polyglot.ast.Node;
 import polyglot.ast.NodeFactory;
@@ -1484,12 +1486,25 @@ public class Inliner extends ContextVisitor {
                                            xnf.CanonicalTypeNode(pos, ret.type()),
                                            xnf.Id(pos, ret.name()) ).localDef(ret));
             }
- //         if (0 < returnCount) {
-            if (true) {
-                newBody.add(xnf.Labeled(pos, xnf.Id(pos, label), xnf.Do(pos, body, xnf.BooleanLit(pos, false).type(xts.Boolean()))));
-            } else {
-                newBody.addAll(body.statements());
+            // A return at the end of the method will have been converted to a
+            // break. It's not needed. Turf it.
+            List<Stmt> bodyStmts = body.statements();
+            if ((bodyStmts.get(bodyStmts.size() - 1) instanceof Branch)) {
+                Branch br = (Branch) bodyStmts.get(bodyStmts.size() - 1);
+                if (br.kind() == Branch.BREAK) {
+                    Id breakLabel = br.labelNode();
+                    if (breakLabel.id().equals(label)) {
+                        List<Stmt> statements = new ArrayList<Stmt>();
+                        for (Stmt stmt : bodyStmts) {
+                            if (stmt != br) {
+                                statements.add(stmt);
+                            }
+                        }
+                        body = xnf.Block(body.position(), statements);
+                    }
+                }
             }
+            newBody.add(xnf.Labeled(pos, xnf.Id(pos, label), body));
             if (ret != null) {
                 Expr rval = xnf.Local(pos, xnf.Id(pos, ret.name())).localInstance(ret.asInstance()).type(ret.type().get());
                 newBody.add(xnf.Return(pos, rval));
@@ -1499,8 +1514,7 @@ public class Inliner extends ContextVisitor {
             return xnf.Block(body.position(), newBody);
         }
 
-        // def m(`x:`T):R=S -> def m(`x:`T)={r:R; L:do{ S[return v/r=v; break
-        // L;]; }while(false); return r;}
+        // def m(`x:`T):R=S -> def m(`x:`T)={r:R; L:{ S[return v/r=v; break L;]; }; return r;}
         private X10MethodDecl visitMethodDecl(X10MethodDecl n) {
             // First check that we are within the right method
             if (n.methodDef() != def)
@@ -1508,8 +1522,7 @@ public class Inliner extends ContextVisitor {
             return (X10MethodDecl) n.body(rewriteBody(n.position(), n.body()));
         }
 
-        // (`x:`T):R=>S -> (`x:`T)=>{r:R; L:do{ S[return v/r=v; break L;];
-        // }while(false); return r;}
+        // (`x:`T):R=>S -> (`x:`T)=>{r:R; L:{ S[return v/r=v; break L;]; }; return r;}
         private Closure visitClosure(Closure n) {
             // First check that we are within the right closure
             if (n.closureDef() != def) return n;

commit 0233be6cae0d436a66d06d5c8343c4a042e92521

Fix build break for optimized builds introduced in r20247.

x10.compiler/src/x10/visit/ExpressionFlattener.java
@@ -250,7 +250,7 @@ public final class ExpressionFlattener extends ContextVisitor {
            return returnNode;
         }
         if (n instanceof Block)
-            return flattenBlock((Block) n, parent instanceof Labeled ?
+            n = flattenBlock((Block) n, parent instanceof Labeled ?
                                                 labelMap.remove(old) : null);
         if (n instanceof Expr) return flattenExpr((Expr) n);
         if (n instanceof Stmt) return flattenStmt((Stmt) n);
@@ -822,7 +822,7 @@ public final class ExpressionFlattener extends ContextVisitor {
 
     /**
      * Flatten a block.
-     * If this is a labeled block, we stick the lables on it because flattenLabled
+     * If this is a labeled block, we stick the labels on it because flattenLabeled
      * is about to rip them off.
      * (Otherwise, this is a no-op, the only block with a value, StmtExpr, has already been handled.)
      * <pre>
@@ -833,12 +833,9 @@ public final class ExpressionFlattener extends ContextVisitor {
      * @return a block with all of its constituent statements flattened.
      */
     private Stmt flattenBlock(Block stmt, List<Labeled> list) {
-        assert (!(stmt instanceof StmtExpr));
-        // return syn.toStmtSeq(stmt);
-
-        // we're about to strip off our parents label in flattenLabled.
+        // we're about to strip off our parents label in flattenLabeled.
         // So, if this block is labeled we'd better cram it on here.
-        return label(stmt,list);
+        return label(stmt, list);
     }
 
     /**