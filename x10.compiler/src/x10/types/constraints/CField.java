sirinath/x10

commit 274b4df58daef273daf123381ebd1c6bbd29e112

lazy creation of the string field used by CField.toString.
git-svn-id: svn://svn.code.sf.net/p/x10/code/trunk@21208 a326200e-df2c-42e0-81fa-597e909af41d

x10.compiler/src/x10/types/constraints/CField.java
@@ -25,20 +25,32 @@ import x10.types.X10MethodDef;
  *
  */
 public class CField  extends XField<Def> {
-    final String string;
+    // lazily initialized
+    private String string;
+    private String getString() {
+        if (string == null) {
+            if (field instanceof MethodDef) {
+                MethodDef fi = (MethodDef)field;
+                string = Types.get(fi.container()) + "#" + fi.name().toString()+ "()";
+            } else {
+                FieldDef fi = (FieldDef)field;
+                string = Types.get(fi.container()) + "#" + fi.name().toString()+ "()";   
+            }
+        }
+        return string;
+    }
+    
     public CField(XVar r, MethodDef fi) {
         this(r, fi, false);
     }
     public CField(XVar r, MethodDef fi, boolean hidden) {
         super(r, fi, hidden);
-        this.string = Types.get(fi.container()) + "#" + fi.name().toString()+ "()";
     }
     public CField(XVar r, FieldDef fi) {
         this(r, fi, false);
     }
     public CField(XVar r, FieldDef fi, boolean hidden) {
         super(r, fi, hidden);
-        this.string = Types.get(fi.container()) + "#" + fi.name().toString();
     }
   
     /**
@@ -79,7 +91,7 @@ public class CField  extends XField<Def> {
   
     @Override
     public String toString() {
-        return (receiver == null ? "" : receiver.toString() + ".") + string;
+        return (receiver == null ? "" : receiver.toString() + ".") + getString();
     }
     
 }

commit 3139142bdfb517d2db794f84dea5f23d2983c245

Fix minor bug introduced in r21208. Normalize SVN properties in x10.compiler/src/x10/types/constraints.

x10.compiler/src/x10/types/constraints/CField.java
@@ -22,9 +22,8 @@ import x10.types.X10MethodDef;
  * An XField with type information (either a MethodDef or a FieldDef).
  * 
  * @author vj
- *
  */
-public class CField  extends XField<Def> {
+public class CField extends XField<Def> {
     // lazily initialized
     private String string;
     private String getString() {
@@ -34,7 +33,7 @@ public class CField  extends XField<Def> {
                 string = Types.get(fi.container()) + "#" + fi.name().toString()+ "()";
             } else {
                 FieldDef fi = (FieldDef)field;
-                string = Types.get(fi.container()) + "#" + fi.name().toString()+ "()";   
+                string = Types.get(fi.container()) + "#" + fi.name().toString();
             }
         }
         return string;