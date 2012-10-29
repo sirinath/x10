sirinath/x10

commit 54d68d837c696513edee32a9a0dda4cf4a07fcd7

I.D Added clocked qualifiers for finish, asyncs.
 FinishStatement ::= clocked finish Statement
 AtEachStatement ::= clocked ateach (LoopIndex in Expression) Statement
 ForEachStatement ::= clocked foreach (LoopIndex in Expression) Statement
 Modifier ::= clocked
 
 EnhancedForStatement ::= for ( Expression ) Statement
 AtEachStatement ::= [clocked] ateach (Expression) Statement
 ForEachStatement ::= [clocked] foreach (Expression) Statement

I.E Added x10.compiler.Det

I.G Removed support for async (P) S. Write async at(P) S or at(P)
async S.

Removed async expressions:
  AsyncExpression ::= async FunctionBody
                     | async PlaceExpressionSingleList FunctionBody
                     | async [ Type ] FunctionBody
                     | async [ Type ] PlaceExpressionSingleList   FunctionBody

Added "variable-less" loops:
  ForLoop ::= foreach ( Expression ) Statement
  ForEachStatement ::= foreach (Expression ) ClockedClauseopt Statement
  AtEachStatement ::= ateach ( Expression )   ClockedClauseopt Statement

Moved Future to x10.util. There is no longer any syntax support for
futures.

I.M Change support for point deconstruction.
The parenthesis notation '('...')' for point decomposition needs to be aligned
with the use of '[' ... ']' for point construction. 

Remove the productions:
      FormalDeclarator ::=
359                        ( IdentifierList ) ResultType
360                       | Identifier ( IdentifierList ) ResultType
      VariableDeclarator ::=
364                         | ( IdentifierList ) HasResultTypeopt = VariableInitializer
365                         | Identifier ( IdentifierList ) HasResultTypeopt = VariableInitializer
     VariableDeclaratorWithType ::= 
367                        ( IdentifierList ) HasResultType = VariableInitializer
368                       | Identifier ( IdentifierList ) HasResultType = VariableInitializer

Add the productions:
      FormalDeclarator ::=

x10.dist/samples/KMeansDist.x10
@@ -49,7 +49,7 @@ public class KMeansDist {
 
             finish {
                 // reset state
-                for (d in points_dist.places()) async(d) {
+                for (d in points_dist.places()) async at(d) {
                     for (var j:Int=0 ; j<DIM*CLUSTERS ; ++j) {
                         local_curr_clusters()(j) = central_clusters(j);
                         local_new_clusters()(j) = 0;
@@ -64,7 +64,7 @@ public class KMeansDist {
                 // compute new clusters and counters
                 for (var p_:Int=0 ; p_<POINTS ; ++p_) {
                     val p = p_;
-                    async (points_dist(p,0)) {
+                    async at(points_dist(p,0)) {
                         var closest:Int = -1;
                         var closest_dist:Float = Float.MAX_VALUE;
                         for (var k:Int=0 ; k<CLUSTERS ; ++k) { 
@@ -96,7 +96,7 @@ public class KMeansDist {
             }
 
             finish {
-                for (d in points_dist.places()) async(d) {
+                for (d in points_dist.places()) async {
                
                     at (Place.FIRST_PLACE) atomic {
                         for (var j:Int=0 ; j<DIM*CLUSTERS ; ++j) {

x10.compiler/src/x10/ast/Async_c.java
@@ -73,11 +73,19 @@ public class Async_c extends Stmt_c implements Async {
 	
 	public Stmt body;
 	protected List<Expr> clocks;
+	protected boolean clocked; // should be equal to (clocks != null && clocks.size() > 0)
 
 	public Async_c(Position pos, List<Expr> clocks, Stmt body) {
 		super(pos);
 		this.clocks = clocks;
 		this.body = body;
+	}
+	public Async_c(Position pos, Stmt body, boolean clocked) {
+		super(pos);
+		this.clocked = true;
+		this.body = body;
+		// temporary. Needs to be initialized with clock from environment.
+		this.clocks = new ArrayList<Expr>();
 	}
 
 	public Async_c(Position p) {

x10.compiler/src/x10/ast/AtEach_c.java
@@ -38,7 +38,7 @@ import x10.types.constraints.CConstraint;
 import x10.types.constraints.XConstrainedTerm;
 
 /**
- * An immutable representation of the X10 statement: ateach (i : D) S
+ * An immutable representation of the X10 statement: [clocked] ateach (i : D) S
  * @author vj Dec 9, 2004
  * @author Christian Grothoff
  */
@@ -61,6 +61,10 @@ public class AtEach_c extends X10ClockedLoop_c implements AtEach, Clocked {
 	 */
 	public AtEach_c(Position pos, Formal formal, Expr domain, List clocks, Stmt body) {
 		super(pos, formal, domain, clocks, body);
+		loopKind=LoopKind.ATEACH;
+	}
+	public AtEach_c(Position pos, Formal formal, Expr domain, Stmt body) {
+		super(pos, formal, domain, body);
 		loopKind=LoopKind.ATEACH;
 	}
 

x10.compiler/src/x10/ast/Finish_c.java
@@ -31,7 +31,7 @@ import polyglot.visit.PrettyPrinter;
 public class Finish_c extends Stmt_c implements Finish {
 
     protected Stmt body;
-    
+    protected boolean clocked;
     /**
      * @param pos
      */
@@ -39,9 +39,10 @@ public class Finish_c extends Stmt_c implements Finish {
 	super(pos);
     }
 
-    public Finish_c(Position pos, Stmt body) {
+    public Finish_c(Position pos, Stmt body, boolean clocked) {
 	super(pos);
 	this.body = body;
+	this.clocked = clocked; 
     }
 
 

x10.compiler/src/x10/ast/ForEach_c.java
@@ -28,7 +28,7 @@ import x10.types.X10Context;
 import x10.types.X10TypeSystem;
 
 /**
- * An immutable representation of the X10 statement: foreach (i : D) S
+ * An immutable representation of the X10 statement: [clocked] foreach (i : D) S
  * @author vj Dec 9, 2004
  * @author Christian Grothoff
  */
@@ -51,6 +51,10 @@ public class ForEach_c extends X10ClockedLoop_c implements ForEach, Clocked {
 	 */
 	public ForEach_c(Position pos, Formal formal, Expr domain, List clocks, Stmt body) {
 		super(pos, formal, domain, clocks, body);
+		loopKind=LoopKind.FOREACH;
+	}
+	public ForEach_c(Position pos, Formal formal, Expr domain, Stmt body) {
+		super(pos, formal, domain,  body);
 		loopKind=LoopKind.FOREACH;
 	}
 

x10.compiler/src/x10/ast/X10ClockedLoop_c.java
@@ -11,6 +11,7 @@
 
 package x10.ast;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
@@ -59,6 +60,13 @@ public abstract class X10ClockedLoop_c extends X10Loop_c implements Clocked {
 	{
 		super(pos, formal, domain, body);
 		this.clocks = TypedList.copyAndCheck(clocks, Expr.class, true);
+	}
+	public X10ClockedLoop_c(Position pos, Formal formal, Expr domain,
+			Stmt body)
+	{
+		super(pos, formal, domain, body);
+		// TODO: The clock had to be obtained from the environment in the desugarer and added here.
+		this.clocks = new ArrayList<Expr>();
 	}
 
 	/** Clocks */

x10.compiler/src/x10/ast/X10FieldDecl_c.java
@@ -229,9 +229,7 @@ public class X10FieldDecl_c extends FieldDecl_c implements X10FieldDecl {
 
     protected FieldDef createFieldDef(TypeSystem ts, ClassDef ct, Flags flags) {
     	X10Flags xFlags = X10Flags.toX10Flags(flags);
-    	if (xFlags.isProperty())
-    		xFlags = xFlags.Global();
-
+    	
     	X10FieldDef fi = (X10FieldDef) ts.fieldDef(position(), Types.ref(ct.asType()), flags, type.typeRef(), name.id());
     	fi.setThisVar(((X10ClassDef) ct).thisVar());
 
@@ -384,9 +382,7 @@ public class X10FieldDecl_c extends FieldDecl_c implements X10FieldDecl {
 	    	Type oldType = (Type)type.copy();
 	    	X10Context xc = (X10Context) enterChildScope(type(), tc.context());
 	    	X10Flags f = X10Flags.toX10Flags(flags.flags());
-	    	if (f.isGlobal() && ! f.isFinal()) {
-	    		Errors.issue(tc.job(), new Errors.GlobalFieldIsVar(this));
-	    	}
+	    	
 	    	try {
                 X10TypeMixin.checkMissingParameters(typeNode);
 	    	} catch (SemanticException e) {

x10.compiler/src/x10/ast/X10Field_c.java
@@ -292,6 +292,7 @@ public class X10Field_c extends Field_c {
 		result.checkConsistency(c);
 
 		checkFieldAccessesInDepClausesAreFinal(result, tc);
+		checkClockedFieldAccessesAreInClockedMethods(result,tc);
 		// Not needed in the orthogonal locality proposal.
 		// result = PlaceChecker.makeFieldAccessLocalIfNecessary(result, tc);
 
@@ -357,6 +358,22 @@ public class X10Field_c extends Field_c {
 		}
 		//System.err.println("X10Field_c: fieldRightType returns " + t);
 		return t;
+	}
+
+	/**
+	 * Check thst if this field is a clocked field, it is being accessed from within a clocked method.
+	 * @param result
+	 * @param tc
+	 * @throws SemanticException
+	 */
+	protected void checkClockedFieldAccessesAreInClockedMethods(X10Field_c result, ContextVisitor tc) 
+	throws SemanticException {
+		//		 Check that field accesses in dep clauses refer to final fields.
+		X10Context xtc = (X10Context) tc.context();
+		if (X10Flags.toX10Flags(result.flags()).isClocked() 
+				&& ! ((X10Context) tc.context()).isClocked()) {
+			throw new Errors.IllegalClockedAccess(this, position());
+		}
 	}
 
 	private static final boolean ENABLE_PLACE_TYPES = true;

commit 9b269d627cd1fbc2508f45a56180864c1bc7f971

Blush:  Correctness of accumulation loop was broken by r16317 way back in 2010 when we removed async (P) S from the grammar.  This code used to have an "async (d)" in it and instead of being converted to "at (d) async" it was changed to just "async" and the place shift got lost.

x10.dist/samples/KMeansDist.x10
@@ -109,7 +109,7 @@ public class KMeansDist {
                 val central_clusters_gr = GlobalRef(central_clusters);
                 val central_cluster_counts_gr = GlobalRef(central_cluster_counts);
                 val there = here;
-                for (d in points_dist.places()) async {
+                for (d in points_dist.places()) at (d) async {
                     // access PlaceLocalHandles 'here' and then data will be captured by at and transfered to 'there' for accumulation
                     val tmp_new_clusters = local_new_clusters();
                     val tmp_cluster_counts = local_cluster_counts();