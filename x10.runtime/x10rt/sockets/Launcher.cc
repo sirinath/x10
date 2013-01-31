sirinath/x10

commit 97cf9f6d6c2d91fd910e70d326b3dff3851fe735

Overhaul of the way places determine the host they are running on, from a query of the socket, to whatever is specified in the hostfile/hostlist.  XTENLANG-3145
git-svn-id: svn://svn.code.sf.net/p/x10/code/trunk@24755 a326200e-df2c-42e0-81fa-597e909af41d

x10.runtime/x10rt/sockets/Launcher.cc
@@ -46,12 +46,9 @@
 /* *********************************************************************** */
 const char* CTRL_MSG_TYPE_STRINGS[] = {"HELLO", "GOODBYE", "PORT_REQUEST", "PORT_RESPONSE"};
 
-int Launcher::setPort(uint32_t place, char* port)
+int Launcher::setPort(uint32_t place, int port)
 {
-	if (port == NULL)
-		return -1;
-
-	if (_singleton == NULL)
+	if (_singleton == NULL) // this call is running outside of the launcher
 	{
 		// send this out to our local launcher
 		if (_parentLauncherControlLink <= 0)
@@ -72,17 +69,23 @@ int Launcher::setPort(uint32_t place, char* port)
 		m.type = HELLO;
 		m.from = place;
 		m.to = place;
-		m.datalen = strlen(port);
+		m.datalen = sizeof(port);
 		int r = TCP::write(_parentLauncherControlLink, &m, sizeof(struct ctrl_msg));
-		if (r <= 0)
-			return -1;
-		TCP::write(_parentLauncherControlLink, port, m.datalen);
+		if (r <= 0) return -1;
+		r = TCP::write(_parentLauncherControlLink, &port, m.datalen);
+		if (r < m.datalen) return -1;
 		#ifdef DEBUG
-			fprintf(stderr, "Runtime %u: sent port data \"%s\" to launcher\n", place, port);
+			fprintf(stderr, "Runtime %u: sent port data \"%i\" to launcher\n", place, port);
 		#endif
 	}
-	else
-		strcpy(_singleton->_runtimePort, port);
+	else // this call is running within the launcher
+	{
+		int lenofName = strlen(_singleton->_runtimePort);
+		snprintf(&_singleton->_runtimePort[lenofName], sizeof(_singleton->_runtimePort)-lenofName, ":%u", port);
+		#ifdef DEBUG
+			fprintf(stderr, "Launcher %u: Runtime is at \"%s\"\n", place, _singleton->_runtimePort);
+		#endif
+	}
 	return 1;
 }
 
@@ -228,6 +231,23 @@ void Launcher::startChildren()
 	if (!_pidlst || !_childControlLinks || !_childCoutLinks || !_childCerrorLinks)
 		DIE("%u: failed in alloca()", _myproc);
 
+	char masterPort[1024];
+	if (_myproc == 0xFFFFFFFF)	// the initial launcher is not in the hostlist.  Get the hostname from the socket
+	{
+		TCP::getname(_listenSocket, masterPort, sizeof(masterPort));
+		setenv(X10_LAUNCHER_HOST, "localhost", 0);
+	}
+	else
+	{ // get hostname from the X10_LAUNCHER_HOST environment variable
+		strcpy(masterPort, getenv(X10_LAUNCHER_HOST));
+		sockaddr_in addr;
+		socklen_t len = sizeof(addr);
+		if (getsockname(_listenSocket, (sockaddr *) &addr, &len) < 0)
+			DIE("failed to get the local socket information");
+		int mplen=strlen(masterPort);
+		snprintf(&masterPort[mplen], sizeof(masterPort)-mplen, ":%u", ntohs(addr.sin_port));
+	}
+
 	for (uint32_t id=0; id <= _numchildren; id++)
 	{
 		int pid, outpipe[2], errpipe[2];
@@ -256,9 +276,6 @@ void Launcher::startChildren()
 			close(errpipe[0]);
 			if (z1 != 1 || z2 != 2)
 				DIE("Launcher %d: DUP failed", _myproc);
-
-			char masterPort[1024];
-			TCP::getname(_listenSocket, masterPort, sizeof(masterPort));
 
 			if (id == _numchildren && _myproc != 0xFFFFFFFF)
 			{ // start up the local x10 runtime
@@ -299,7 +316,7 @@ void Launcher::startChildren()
                     // launch this runtime in a debugger
                     char** newargv;
                     #ifdef DEBUG
-                        fprintf(stderr, "Runtime %u forked with jdb at port %d.  Running exec.\n", _myproc, base_port);
+                        fprintf(stderr, "Runtime %u forked with jdb at port %l.  Running exec.\n", _myproc, base_port);
                     #endif
                     int numArgs = 0;
                     while (_argv[numArgs] != NULL)
@@ -719,10 +736,12 @@ void Launcher::handleNewChildConnection(void)
 			_childControlLinks[_numchildren] = fd;
 			if (m.datalen > 0)
 			{
-				_runtimePort[m.datalen] = '\0';
-				size = TCP::read(_childControlLinks[_numchildren], _runtimePort, m.datalen);
+				int portNum;
+				size = TCP::read(_childControlLinks[_numchildren], &portNum, m.datalen);
 				if (size < m.datalen)
-					DIE("Launcher %u: could not read local runtime data", _myproc);
+					DIE("Launcher %u: could not read local runtime port number", _myproc);
+				int len=strlen(_runtimePort);
+				snprintf(&_runtimePort[len], sizeof(_runtimePort)-len, ":%u", ntohs(portNum));
 			}
 			#ifdef DEBUG
 				fprintf(stderr, "Launcher %u: new ctrl conn %d from local runtime, with runtime port=\"%s\"\n", _myproc, fd, _runtimePort);
@@ -1303,6 +1322,7 @@ void Launcher::startSSHclient(uint32_t id, char* masterPort, char* remotehost)
     argv[++z] = alloc_env_always_assign(X10_LAUNCHER_SSH, _ssh_command);
     argv[++z] = alloc_env_always_assign(X10_LAUNCHER_PARENT, masterPort);
     argv[++z] = alloc_env_always_assign(X10_LAUNCHER_PLACE, alloc_printf("%d",id));
+    argv[++z] = alloc_env_always_assign(X10_LAUNCHER_HOST, remotehost);
 	argv[++z] = cmd;
 	for (int i = 1; i < _argc; i++) {
         argv[++z] = escape_various_things2(_argv[i]);

x10.runtime/x10rt/sockets/Launcher.h
@@ -38,6 +38,7 @@
 #define X10_LAUNCHER_PARENT "X10_LAUNCHER_PARENT" // the hostname:port of the parent launcher.  This is set by the launcher.
 #define X10_LAUNCHER_RUNLAUNCHER "X10_LAUNCHER_RUNLAUNCHER" // this is a flag to run as a runtime or a launcher.  Set by the launcher.
 #define X10_LAUNCHER_CWD "X10_LAUNCHER_CWD" // the working directory of the program
+#define X10_LAUNCHER_HOST "X10_LAUNCHER_HOST" // the hostname from the hostfile or hostlist
 #define X10_SCRATCH_CWD "X10_SCRATCH_CWD" // Make a new (empty) cwd directory for each place to run in
 #define X10_GDB "X10_GDB" // This flag causes the runtime to launch under gdb in a new xterm
 #define X10_JDB "X10_JDB" // This flag causes the runtime to add extra parameters that java interprets to turn on jdb support in the vm 
@@ -81,7 +82,7 @@ class Launcher
 		static void cb_sighandler_cld(int signo);
 		static void cb_sighandler_term(int signo);
 		static int lookupPlace(uint32_t myPlace, uint32_t destPlace, char* response, int responseLen);
-		static int setPort(uint32_t place, char* port);
+		static int setPort(uint32_t place, int port);
 
 	protected:
 		/* SockProcManager_Init.cc */
@@ -120,7 +121,7 @@ class Launcher
 
 		/* parent child structure */
 		char ** _hostlist; /* child host list */
-		char _runtimePort[512]; /* the host:port number of the associated x10 runtime's listen port */
+		char _runtimePort[1024]; /* the host:port number of the associated x10 runtime's listen port */
 		uint32_t _firstchildproc; /* the ID of the first child launcher */
 		uint32_t _numchildren; /* number of launcher children in this node */
 		int * _pidlst; /* list of all spawned pids */

x10.runtime/x10rt/sockets/Launcher_Init.cc
@@ -116,7 +116,12 @@ void Launcher::initialize(int argc, char ** argv)
 	if (!getenv(X10_LAUNCHER_PLACE))
 		_myproc = 0xFFFFFFFF;
 	else
+	{
 		_myproc = atoi(getenv(X10_LAUNCHER_PLACE));
+		char* host = getenv(X10_LAUNCHER_HOST);
+		if (host) strcpy(_runtimePort, host);
+		else strcpy(_runtimePort, "localhost");
+	}
 
 	/* -------------------------------------------- */
 	/*  decide who my children are                  */

x10.runtime/x10rt/sockets/x10rt_sockets.cc
@@ -85,7 +85,6 @@ struct x10SocketState
 	x10rt_place myPlaceId; // which place we're at.  Also used as the index to the array below. Local per-place memory is used.
 	x10SocketCallback* callBackTable; // I'm told message ID's run from 0 to n, so a simple array using message indexes is the best solution for this table.  Local per-place memory is used.
 	x10rt_msg_type callBackTableSize; // length of the above array
-	char* myhost; // my own hostname, so I can detect places that are on the same machine and use localhost instead.
 	bool yieldAfterProbe; // a little flag that adds sched_yield() after probe, for better performance when there are more workers than processors on a machine (or when debugging).
 	bool linkAtStartup; // this flag tells us that we should establish all our connections at startup, not on-demand.  It gets flipped after all links are up.
 	pthread_mutex_t readLock; // a lock to prevent overlapping reads on each socket
@@ -484,16 +483,6 @@ int initLink(uint32_t remotePlace)
 					if (getenv(X10_HOSTFILE)) fprintf(stderr, "WARNING: "X10_HOSTFILE" is ignored when using "X10_FORCEPORTS);
 				}
 			}
-
-			// check to see if the host is our host, and if so, change it to "localhost"
-			// to take advantage of any localhost OS efficiencies
-			if (strcmp(state.myhost, link) == 0)
-			{
-				strcpy(link, "localhost\0");
-				#ifdef DEBUG
-					fprintf(stderr, "X10rt.Sockets: Place %u changed hostname for place %u to %s\n", state.myPlaceId, remotePlace, link);
-				#endif
-			}
 		}
 
 		int newFD;
@@ -579,7 +568,6 @@ void x10rt_net_init_as_library (int * argc, char ***argv, x10rt_msg_type *counte
 	state.linkAtStartup = false;
 	state.useNonblockingLinks = !checkBoolEnvVar(getenv(X10_NOWRITEBUFFER));
 	state.pendingWrites = NULL;
-	state.myhost = NULL;
 	if (state.useNonblockingLinks)
 		pthread_mutex_init(&state.pendingWriteLock, NULL);
 	pthread_mutex_init(&state.readLock, NULL);
@@ -705,27 +693,24 @@ void x10rt_net_init (int * argc, char ***argv, x10rt_msg_type *counter)
 	pthread_mutex_init(&state.writeLocks[state.myPlaceId], NULL);
 	state.socketLinks[state.myPlaceId].events = POLLIN | POLLPRI;
 
-	char portname[1024];
-	TCP::getname(state.socketLinks[state.myPlaceId].fd, portname, sizeof(portname));
 	if (useLauncher)
 	{   // Tell our launcher our communication port number
+		sockaddr_in addr;
+		socklen_t len = sizeof(addr);
+		if (getsockname(state.socketLinks[state.myPlaceId].fd, (sockaddr *) &addr, &len) < 0)
+			error("failed to get the local socket information");
+
 		pthread_mutex_lock(&state.writeLocks[state.myPlaceId]);
-		if (Launcher::setPort(state.myPlaceId, portname) < 0)
+		if (Launcher::setPort(state.myPlaceId, addr.sin_port) < 0)
 			error("failed to connect to the local runtime");
 		pthread_mutex_unlock(&state.writeLocks[state.myPlaceId]);
 	}
-
-	// save our hostname for later
-	char * c = strchr(portname, ':');
-	c[0] = '\0';
-	state.myhost = (char*)malloc(strlen(portname)+1);
-	strcpy(state.myhost, portname);
 
 	state.pendingWrites = NULL;
 	if (state.useNonblockingLinks)
 		pthread_mutex_init(&state.pendingWriteLock, NULL);
 	#ifdef DEBUG
-		fprintf(stderr, "X10rt.Sockets: place %u running on %s\n", state.myPlaceId, state.myhost);
+		fprintf(stderr, "X10rt.Sockets: place %u running\n", state.myPlaceId);
 	#endif
 }
 
@@ -1229,7 +1214,6 @@ void x10rt_net_finalize (void)
 		#endif
 	}
 	pthread_mutex_destroy(&state.readLock);
-	if (state.myhost) free(state.myhost);
 	free(state.socketLinks);
 	free(state.writeLocks);
 }

commit 5a5512ab938db231a80272ce225f1a2fdc652563

Fixed a race condition which showed up at larger scales.  The race was introduced in r24755.

x10.runtime/x10rt/sockets/Launcher.cc
@@ -953,7 +953,7 @@ int Launcher::handleControlMessage(int fd)
 		{
 			case PORT_REQUEST:
 			{
-				while (_runtimePort[0] == '\0')
+				while (strchr(_runtimePort, ':') == NULL)
 				{
 					sched_yield();
 					handleRequestsLoop(true);