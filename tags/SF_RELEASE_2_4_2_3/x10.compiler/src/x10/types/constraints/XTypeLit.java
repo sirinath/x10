850f04828eacdf806c9529515b29bab2d95034a3
dgrove-oss <dgrove-oss@a326200e-df2c-42e0-81fa-597e909af41d>
2014-03-20 14:40:06 +0000

    delete & retag 2.4.2.3

--- tags/SF_RELEASE_2_4_2_3/x10.compiler/src/x10/types/constraints/XTypeLit.java
+++
@@ -1,8 +0,0 @@
-package x10.types.constraints;
-
-import x10.constraint.XLit;
-import polyglot.types.Type;
-
-public interface XTypeLit extends XLit {
-	public Type type();
-}
--- tags/SF_RELEASE_2_4_2_3/x10.gml/src/x10/matrix/comm/DistDataArray.x10
+++
@@ -1,5 +0,0 @@
-package x10.matrix.comm;
-
-import x10.regionarray.DistArray;
-
-public type DistDataArray = DistArray[Rail[Double]](1);
