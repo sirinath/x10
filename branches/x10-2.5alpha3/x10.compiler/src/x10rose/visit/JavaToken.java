77500730ce56e5e356a5da5ac89dcd75c5a3e366
dgrove-oss <dgrove-oss@a326200e-df2c-42e0-81fa-597e909af41d>
2014-08-08 13:47:21 +0000

    Copying X10 trunk revision 28049 as x10-2.5alpha3 branch of X10

---
+++ branches/x10-2.5alpha3/x10.compiler/src/x10rose/visit/JavaToken.java
@@ -0,0 +1,30 @@
+package x10rose.visit;
+
+/**
+ * This is the token class in Java that we will hand as an object to the C++ JNI functions.
+ * We also wrap source position information here.
+ */
+public class JavaToken {
+    // We mostly just want the token to carry a string version
+    // of the parsed code and source position.
+    public String text;   
+    public String filename;
+    private JavaSourcePositionInformation posInfo;
+
+    public JavaToken(String s, JavaSourcePositionInformation posInfo) {
+        this.text = s;
+        this.posInfo = posInfo;
+    }
+
+    public String getText() {
+        return text;
+    }
+    
+    public String getFileName() {
+        return filename;
+    }
+
+	 public JavaSourcePositionInformation getJavaSourcePositionInformation() {
+        return this.posInfo;
+    }
+}
